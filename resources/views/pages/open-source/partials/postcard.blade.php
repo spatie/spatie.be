1a4656b9546d72684b6a5af906bf5465b43ca5ba
Willem Van Bockstal <willem@spatie.be>
2018-07-20 17:44:03 +0200

    Smaller postcards

--- resources/views/pages/open-source/partials/postcard.blade.php
+++ resources/views/pages/open-source/partials/postcard.blade.php
@@ -1,13 +1,13 @@
 <div class="illustration is-postcard" style="left: {{ rand(-20, +20) }}px; top: {{ rand(-20, +10) }}px">
     {{ $postcard->getFirstMedia() }}
 
-    <div class="mt-4 text-sm links-underline links-black">
+    <div class="mt-4 text-xs links-underline links-black leading-tight">
         @if($postcard->sender)
             {!! $postcard->sender !!}
         @endif
 
         @if ($postcard->location)
-            <div class="flex items-start text-xs text-grey">
+            <div class="flex items-start text-grey mt-1">
                 <i class="fas flex-none fa-map-marker-alt text-grey-lighter"></i>
                 <span class="ml-2">{{ $postcard->location }}</span>
             </div>
