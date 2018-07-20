3cfc7bc2c771305a71f003b557b43e38ac33e88f
Willem Van Bockstal <willem@spatie.be>
2018-07-20 17:55:20 +0200

    Postcards text tweak

--- resources/assets/js/front/ui/zoom.js
+++ resources/assets/js/front/ui/zoom.js
@@ -1,7 +1,7 @@
 import { zoom } from '@willemvb/zoom.js/src/zoom.js';
 
 // zoom has little use on 1 col
-if (document.body.clientWidth >= 704) {
+if (document.body.clientWidth >= 383) {
     document.addEventListener('DOMContentLoaded', () => {
         const elems = document.querySelectorAll('[data-action="zoom"] img');
         for (let i = 0; i < elems.length; i++) {
--- resources/views/pages/open-source/partials/postcards.blade.php
+++ resources/views/pages/open-source/partials/postcards.blade.php
@@ -1,5 +1,5 @@
 <section id="postcards" class="section pt-0 -mt-8 overflow-visible">
-    <div class="wrap-gallery" data-action="zoom">
+    <div class="wrap-gallery items-start" data-action="zoom">
         @foreach($postcards as $postcard)
                 @include('pages.open-source.partials.postcard')
         @endforeach
