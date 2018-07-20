8f8d0d9b9fdb11720588bd8cc2949118b49b4de5
Willem Van Bockstal <willem@spatie.be>
2018-07-20 17:31:30 +0200

    Responsive image tweaks

--- resources/views/pages/open-source/partials/postcard.blade.php
+++ resources/views/pages/open-source/partials/postcard.blade.php
@@ -1,14 +1,15 @@
 <div class="illustration is-postcard" style="left: {{ rand(-20, +20) }}px; top: {{ rand(-20, +10) }}px">
     {{ $postcard->getFirstMedia() }}
 
-    <div class="mt-4 links-underline links-black">
+    <div class="mt-4 text-sm links-underline links-black">
         @if($postcard->sender)
             {!! $postcard->sender !!}
         @endif
 
         @if ($postcard->location)
-            <div class="text-sm text-grey">
-                <i class="fas fa-map-marker-alt text-grey-lighter"></i> {{ $postcard->location }}
+            <div class="flex items-start text-xs text-grey">
+                <i class="fas flex-none fa-map-marker-alt text-grey-lighter"></i>
+                <span class="ml-2">{{ $postcard->location }}</span>
             </div>
         @endif
     </div>
