cad7e7e6822a223982afe9921eec991239e1467a
Willem Van Bockstal <willem@spatie.be>
2018-07-21 00:03:19 +0200

    Remove character

--- resources/views/layout/partials/wallpaper.blade.php
+++ resources/views/layout/partials/wallpaper.blade.php
@@ -2,6 +2,5 @@
     <div class="wallpaper">
         {{-- wallpaper is always 100vw, so we don't need pragmatic srcset sizes --}}
         <img srcset="{{ image($background)->getSrcset() }}" src="{{ image($background)->getUrl() }}" width="2400" sizes="100vw">
-        }
     </div>
 @endisset
