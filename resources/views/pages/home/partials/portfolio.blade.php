e06bf99c5831de163709958f762da1d3855e8c63
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-07-25 15:04:26 +0200

    Link targets

--- resources/views/pages/home/partials/portfolio.blade.php
+++ resources/views/pages/home/partials/portfolio.blade.php
@@ -14,19 +14,19 @@
                     </div>
                 </h3>
                 <p class="text-lg">
-                    For Martin Garrix' record label <a href="https://stmpdrcrds.com">stmpdcrds.com</a> we made a workflow application to streamline their demo track influx.
+                    For Martin Garrix' record label <a href="https://stmpdrcrds.com" target="_blank" rel="nofollow noreferrer noopener">stmpdcrds.com</a> we made a workflow application to streamline their demo track influx.
                     <br>
                     Crew members can listen and evaluate SoundCloud tracks by upcoming talents, submitted to the application.
                 </p>
                 <ul class="text-lg mt-8">
-                    <li><span class="icon fill-green-dark">{{ svg('icons/far-angle-right') }}</span> <a href="https://demo.stmpdrcrds.com">demo.stmpdrcrds.com</a></li>
+                    <li><span class="icon fill-green-dark">{{ svg('icons/far-angle-right') }}</span> <a href="https://demo.stmpdrcrds.com" target="_blank" rel="nofollow noreferrer noopener">demo.stmpdrcrds.com</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="wrap-8 grid-flow-dense section">
         <div class="sm:startx-5 sm:spanx-4">
-            <a href="https://my.artassistant.com" class="illustration is-right h-full" title="my.artassistant.com">
+            <a href="https://my.artassistant.com" target="_blank" rel="nofollow noreferrer noopener" class="illustration is-right h-full" title="my.artassistant.com">
                 {{ image('portfolio/artassistant.jpg') }}
             </a>
         </div>
@@ -43,7 +43,7 @@
                     We assisted this startup with the development of a 'collection manager' that facilitates selling and exposing artwork as a gallery owner.
                 </p>
                 <ul class="text-lg mt-8 inline-block">
-                    <li><span class="icon fill-pink-dark">{{ svg('icons/far-angle-right') }}</span> <a href="https://my.artassistant.com">my.artassistant.com</a></li>
+                    <li><span class="icon fill-pink-dark">{{ svg('icons/far-angle-right') }}</span> <a href="https://my.artassistant.com" target="_blank" rel="nofollow noreferrer noopener">my.artassistant.com</a></li>
                 </ul>
             </div>
         </div>
