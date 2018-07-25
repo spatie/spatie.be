34a15fab65ee98bfa58542ed9110271a44ba27fd
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-07-25 13:52:28 +0200

    App js cleanup

--- resources/assets/js/front/app.js
+++ resources/assets/js/front/app.js
@@ -3,12 +3,4 @@ import repositories from './repositories';
 
 window.addEventListener('load', images);
 
-// window.addEventListener('resize', () => {
-//     clearTimeout(resizeTimer);
-
-//     resizeTimer = setTimeout(() => {
-//         images();
-//     }, 250);
-// });
-
 [...document.querySelectorAll('[data-repositories]')].forEach(repositories);
