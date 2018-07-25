20cb4f623ce9067899cbe415f9c298384c438007
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-07-25 10:27:58 +0200

    Image perf

--- resources/views/pages/home/partials/clients.blade.php
+++ resources/views/pages/home/partials/clients.blade.php
@@ -5,13 +5,13 @@
         </h2>
     </div>
     <div class="wrap-logos mt-8">
-        <a href="https://mutsy.com" target="_blank" rel="noreferrer noopener"><img src="/images/clients/mutsy.svg"></a>
-        <a href="https://krauthammer.com" target="_blank" rel="noreferrer noopener"><img src="/images/clients/krauthammer.svg"></a>
-        <a href="https://vlaanderen.be" target="_blank" rel="noreferrer noopener"><img src="/images/clients/vlaanderen.svg"></a>
-        <a href="https://flux50.com" target="_blank" rel="noreferrer noopener"><img src="/images/clients/flux50.svg"></a>
-        <a href="https://skylineeurope.be" target="_blank" rel="noreferrer noopener"><img src="/images/clients/skyline.svg"></a>
-        <a href="https://bodartservicehouse.be" target="_blank" rel="noreferrer noopener"><img src="/images/clients/bodart.svg"></a>
-        <a href="https://vluchtelingenwerk.be" target="_blank" rel="noreferrer noopener"><img src="/images/clients/vluchtelingenwerk.svg"></a>
-        <a href="https://vrijwilligerswerk.be" target="_blank" rel="noreferrer noopener"><img src="/images/clients/vrijwilligerswerk.svg"></a>
+        <a href="https://mutsy.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/mutsy') }}</a>
+        <a href="https://krauthammer.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/krauthammer') }}</a>
+        <a href="https://vlaanderen.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vlaanderen') }}</a>
+        <a href="https://flux50.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/flux50') }}</a>
+        <a href="https://skylineeurope.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/skyline') }}</a>
+        <a href="https://bodartservicehouse.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/bodart') }}</a>
+        <a href="https://vluchtelingenwerk.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vluchtelingenwerk') }}</a>
+        <a href="https://vrijwilligerswerk.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vrijwilligerswerk') }}</a>
     </div>
 </section>
