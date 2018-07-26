bf9871c78c1a0cff82b13dd68c0b6bbe18832d99
Willem Van Bockstal <willem@spatie.be>
2018-07-26 13:12:58 +0200

    Add serviceworker.js

--- resources/assets/js/front/app.js
+++ resources/assets/js/front/app.js
@@ -4,3 +4,9 @@ import repositories from './repositories';
 window.addEventListener('load', images);
 
 [...document.querySelectorAll('[data-repositories]')].forEach(repositories);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/serviceworker.js');
+  });
+}
