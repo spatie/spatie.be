56b7f24658b2ca4bee516bf0d5b1e7686edbab57
Willem Van Bockstal <willem@spatie.be>
2018-08-28 10:16:28 +0200

    Mention Laravel Nova; correction

--- resources/views/pages/laravel/partials/hire.blade.php
+++ resources/views/pages/laravel/partials/hire.blade.php
@@ -9,7 +9,7 @@
                     <a href="{{ route('about') }}#team">Our team</a> is a fine blend of back &amp; frontend profiles that are used to working together, tightly coupled.
                 </p>
                 <p class="text-lg">
-                    We assist agencies in the scaffolding of Laravel projects. We can also help you out with your own Laravel or Vue packages, custom Laravel Nova components or build a Saas based on Laravel Spark.
+                    We assist agencies in the scaffolding of Laravel projects. We can also help you out with your own Laravel or Vue packages, custom Laravel Nova components or a Saas based on Laravel Spark.
                 </p>
                 <div class="inset-green mt-16">
                     <p class="text-2xl links-white links-underline">
