f507ccdcf742057f446c3cb6db7707c0e10d9c6e
Willem Van Bockstal <willem@spatie.be>
2018-10-01 17:10:24 +0200

    Add layout

---
+++ resources/views/pages/open-source/partials/patreon.blade.php
@@ -0,0 +1,23 @@
+<div class="flex items-center mt-8 my-6">
+    <div class="avatar">
+        <img src="">
+    </div>
+    <div class="ml-4">
+        <h3 class="title-sm">
+            Patreon name
+        </h3>
+        <p class="text-xs text-grey mt-2 links-underline links-grey">
+            Thank you for your pledge
+
+            {{--
+                Random praise phrase selection for every patreon?
+
+                    - Thank your for your pledge
+                    - You sir/madam are awesome
+                    - We eat our monthly pasta thanks to you
+                    - Your actions are heart-warming
+
+                --}}
+        </p>
+    </div>
+</div>
