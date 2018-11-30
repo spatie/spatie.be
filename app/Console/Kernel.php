0acb29087d4bc2846993e0d2077f2702aaba7bdf
Ruben Van Assche <ruben@spatie.be>
2018-11-30 15:18:49 +0100

    Add patreon command to kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('import:random-contributor')->hourly();
         $schedule->command('import:packagist-downloads')->hourly();
         $schedule->command('import:github-repositories')->daily();
+        $schedule->command('import:patreon-pledgers')->daily();
     }
 
     protected function commands()
