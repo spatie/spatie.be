059625652792db482064457af80f3abd847fdc42
Willem Van Bockstal <willem@spatie.be>
2018-12-03 13:13:22 +0100

    Add backend vacancy

--- resources/views/pages/vacancies/partials/list.blade.php
+++ resources/views/pages/vacancies/partials/list.blade.php
@@ -9,16 +9,16 @@
 </p>
 --}}
 <p class="mt-4">
+    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'backend-developer') }}">Backend developer</a>
+    <br>
+    <span class="text-xs text-grey">Antwerp / Partially remote</span>
+</p>
+<p class="mt-4">
     <a class="link-underline link-blue" href="{{ route('vacancies.show', 'spontaneous-application') }}">Spontaneous application</a>
     <br>
     <span class="text-xs text-grey">Antwerp / Partially remote</span>
 </p>
 {{--
-<p class="mt-4">
-    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'backend-developer') }}">Backend developers</a>
-    <br>
-    <span class="text-xs text-grey">Antwerp / Partially remote</span>
-</p>
 <p class="mt-4">
     <a class="link-underline link-blue" href="{{ route('vacancies.show', 'front-end-developer') }}">Frontend Developer</a>
     <br>
