892a0b1b32c45ff92b7a8f6c0a0d4ff214b10613
Willem Van Bockstal <willem@spatie.be>
2019-08-30 17:10:54 +0200

    Add Flare to homepage

--- resources/views/pages/home/partials/portfolio.blade.php
+++ resources/views/pages/home/partials/portfolio.blade.php
@@ -2,11 +2,41 @@
     <div class="wrap-8 section">
         <div class="sm:spanx-4 | md:startx-2 md:spanx-3 md:-ml-32 md:mr-16">
             <a href="https://demo.stmpdrcrds.com" class="illustration is-left h-full" title="demo.stmpdrcrds.com">
-                {{ image('portfolio/stmpd.jpg') }}
+                {{ image('portfolio/flare.jpg') }}
             </a>
         </div>
         <div class="sm:spanx-3 sm:startx-5 | self-center">
             <div class="markup links-blue links-underline">
+                <h3 class="title">
+                    Ignition + Flare
+                    <div class="title-subtext text-pink-dark">
+                        Error page<span class="font-normal"> + </span>tracker <span class="font-normal">for
+                            Laravel</span>
+                    </div>
+                </h3>
+                <p class="text-lg">
+                    Ignition is a free and shareable error page that ships with Laravel 6 and up. It's built to keep you
+                    focused on finding a solution.
+                    It also couples with an online service —Flare— that tracks production errors and let you work in
+                    team to fix them.
+                </p>
+                <ul class="text-lg mt-8">
+                    <li><span class="icon fill-pink-dark">{{ svg('icons/far-angle-right') }}</span> <a
+                            href="https://flareapp.io" target="_blank"
+                            rel="nofollow noreferrer noopener">flareapp.io</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrap-8 grid-flow-dense section">
+        <div class="sm:startx-5 sm:spanx-4 | md:spanx-3 md:ml-16 md:-mr-32">
+            <a href="https://demo.stmpdrcrds.com" class="illustration is-left h-full" title="demo.stmpdrcrds.com">
+                {{ image('portfolio/stmpd.jpg') }}
+            </a>
+        </div>
+        <div class="sm:startx-2 sm:spanx-3 | self-center">
+            <div class="markup links-blue links-underline | sm:grid-text-right">
                 <h3 class="title">
                     Soundcloud Demo Platform
                     <div class="title-subtext text-green-dark">
@@ -14,24 +44,31 @@
                     </div>
                 </h3>
                 <p class="text-lg">
-                    For Martin Garrix' record label <a href="https://stmpdrcrds.com" target="_blank" rel="nofollow noreferrer noopener">stmpdrcrds.com</a> we made a workflow application to streamline their demo track influx.
+                    For Martin Garrix' record label <a href="https://stmpdrcrds.com" target="_blank"
+                        rel="nofollow noreferrer noopener">stmpdrcrds.com</a> we made a workflow application to
+                    streamline their demo track influx.
                     <br>
-                    Crew members can listen and evaluate SoundCloud tracks by upcoming talents, submitted to the application.
+                    Crew members can listen and evaluate SoundCloud tracks by upcoming talents, submitted to the
+                    application.
                 </p>
                 <ul class="text-lg mt-8">
-                    <li><span class="icon fill-green-dark">{{ svg('icons/far-angle-right') }}</span> <a href="https://demo.stmpdrcrds.com" target="_blank" rel="nofollow noreferrer noopener">demo.stmpdrcrds.com</a></li>
+                    <li><span class="icon fill-green-dark">{{ svg('icons/far-angle-right') }}</span> <a
+                            href="https://demo.stmpdrcrds.com" target="_blank"
+                            rel="nofollow noreferrer noopener">demo.stmpdrcrds.com</a></li>
                 </ul>
             </div>
         </div>
     </div>
-    <div class="wrap-8 grid-flow-dense section">
-        <div class="sm:startx-5 sm:spanx-4 | md:spanx-3 md:ml-16 md:-mr-32">
-            <a href="https://my.artassistant.com" target="_blank" rel="nofollow noreferrer noopener" class="illustration is-right h-full" title="my.artassistant.com">
+
+    <div class="wrap-8 section">
+        <div class="sm:spanx-4 | md:startx-2 md:spanx-3 md:-ml-32 md:mr-16">
+            <a href="https://my.artassistant.com" target="_blank" rel="nofollow noreferrer noopener"
+                class="illustration is-right h-full" title="my.artassistant.com">
                 {{ image('portfolio/artassistant.jpg') }}
             </a>
         </div>
-        <div class="sm:startx-2 sm:spanx-3 | self-center">
-            <div class="markup links-blue links-underline | sm:grid-text-right">
+        <div class="sm:spanx-3 sm:startx-5 | self-center">
+            <div class="markup links-blue links-underline">
                 <h3 class="title">
                     Art Transaction Platform
                     <div class="title-subtext text-pink-dark">
@@ -39,11 +76,15 @@
                     </div>
                 </h3>
                 <p class="text-lg">
-                    ArtAssistant is the one-stop shop for the world of art, offering different services such as valuation, trading and lending of artworks.
-                    We assisted this startup with the development of a 'collection manager' that facilitates selling and exposing artwork as a gallery owner.
+                    ArtAssistant is the one-stop shop for the world of art, offering different services such as
+                    valuation, trading and lending of artworks.
+                    We assisted this startup with the development of a 'collection manager' that facilitates selling and
+                    exposing artwork as a gallery owner.
                 </p>
                 <ul class="text-lg mt-8 inline-block">
-                    <li><span class="icon fill-pink-dark">{{ svg('icons/far-angle-right') }}</span> <a href="https://my.artassistant.com" target="_blank" rel="nofollow noreferrer noopener">my.artassistant.com</a></li>
+                    <li><span class="icon fill-pink-dark">{{ svg('icons/far-angle-right') }}</span> <a
+                            href="https://my.artassistant.com" target="_blank"
+                            rel="nofollow noreferrer noopener">my.artassistant.com</a></li>
                 </ul>
             </div>
         </div>
