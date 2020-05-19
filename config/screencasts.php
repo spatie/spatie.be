04f24b4fbbfb56f653d1a5d23806bbe942d9900f
freek <freek@spatie.be>
2020-05-19 10:42:18 +0200

    wip

--- config/screencasts.php
+++ config/screencasts.php
@@ -14,5 +14,17 @@ return [
                 ['id' => '417228265', 'is_paid' => true], // creating a new release
             ]
         ],
+        [
+            'title' => 'Another series',
+            'screencasts' => [
+                ['id' => '417225584', 'is_paid' => false], // intro
+                ['id' => '417225712', 'is_paid' => true], // starting a php package
+                ['id' => '417226543', 'is_paid' => true], // configuring the namespace
+                ['id' => '417226860', 'is_paid' => true], // running tests on Github Actions
+                ['id' => '417227634', 'is_paid' => true], // enforcing a code style
+                ['id' => '417227966', 'is_paid' => true], // publishing a package
+                ['id' => '417228265', 'is_paid' => true], // creating a new release
+            ]
+        ]
     ]
 ];
