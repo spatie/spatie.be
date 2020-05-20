6bc17ab4ba1591dea92aa83647c3c5be973ef3a0
Willem Van Bockstal <willem@spatie.be>
2020-05-20 15:02:29 +0200

    Video layout

--- resources/views/layout/partials/status.blade.php
+++ resources/views/layout/partials/status.blade.php
@@ -1,8 +1,9 @@
 <div class="py-2 px-8 md:px-16 flex items-center justify-end bg-blue-darker links-white links-underline text-white text-xs">
-    <span class="opacity-75">
-    Logged in as sponsor willemvb</span>
     <span class="opacity-75 mx-2 inline-block align-center w-4 fill-pink">
         {{ svg('icons/fas-heart') }}
     </span>
-    <a class="opacity-75" href="">Log out</a>
+    <span class="opacity-75">
+        Thanks for being our sponsor, willemvb!
+        <a class="ml-1 inline-block" href="">Log out</a>
+    </span>
 </div>
