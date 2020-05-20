e2197b9c8239aa82cb158f952f1b323973d2ba32
Willem Van Bockstal <willem@spatie.be>
2020-05-20 17:29:51 +0200

    Not a sponsor error

--- resources/views/layout/partials/status.blade.php
+++ resources/views/layout/partials/status.blade.php
@@ -10,6 +10,19 @@
             @csrf
             <button type="submit" class="ml-1 px-2 py-1 rounded-full inline-block bg-blue-darkest">Log out</button>
         </form>
-    </span>
     </div>
 @endauth
+
+@if(session()->has('not-a-sponsor'))
+    <div class="py-2 px-8 md:px-16 flex items-center justify-end bg-red links-white links-underline text-white text-xs">
+        <span class="mx-2 inline-block align-center w-4 fill-white">
+            {{ svg('github') }}
+        </span>
+        <span class="opacity-75">
+            Aaaaw… you're not a sponsor yet!
+        </span>
+        <a class="ml-2 link-white link-underline" href="https://github.com/sponsors/spatie" target="_blank">                                             
+            Become a GitHub Sponsor
+        </a>
+    </div>
+@endif
