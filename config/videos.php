b97b1d248f71737e28e32c3b2c57fc687badb1c5
freek <freek@spatie.be>
2020-05-20 14:31:05 +0200

    wip

--- config/videos.php
+++ config/videos.php
@@ -6,7 +6,7 @@ return [
             'title' => 'Source diving SPATIE packages',
             'description' => "Have you ever wondered how some of our packages work under the hood? In this series all secrets will be revealed",
             'videos' => [
-                ['id' => '417225712', 'only_for_sponsors' => true], // starting a php package
+                ['id' => '419946519', 'only_for_sponsors' => true], // multitenancy
                 ['id' => '419946519', 'only_for_sponsors' => true], // response cache
             ],
         ],
