295ee7794f9be433ac9e3ae53c661808cb5dc4b7
Willem Van Bockstal <willem@spatie.be>
2020-05-20 16:57:18 +0200

    Disable service worker

--- resources/js/front/app.js
+++ resources/js/front/app.js
@@ -2,6 +2,8 @@ import images from './images';
 
 window.addEventListener('load', images);
 
+/*
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
 }
+*/
