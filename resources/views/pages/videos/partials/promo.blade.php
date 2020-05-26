4bf134babc51eb4649dfd4a09141b902f4701213
Willem Van Bockstal <willem@spatie.be>
2020-05-26 12:39:15 +0200

    Content changes

--- resources/views/pages/videos/partials/promo.blade.php
+++ resources/views/pages/videos/partials/promo.blade.php
@@ -1,10 +1,10 @@
-<div class="inset-green">
+<div class="inset-green mx-0">
     <div class="wrap-inset md:items-start" style="--cols: auto 1fr">
-        <a class="font-sans-bold bg-green-dark hover:bg-green justify-center flex items-center px-6 py-2 rounded-full text-white text-xl" href="https://laravelpackage.training">
+        <a class="font-sans-bold bg-white hover:text-green-dark justify-center flex items-center px-6 py-2 rounded-full text-green text-xl" href="https://laravelpackage.training">
             Get all videos
         </a>
         <div>
-            <p class="text-lg">
+            <p class="text-base">
                 These videos are part of a <strong>premium course on package development</strong>. As a sponsor, you can get all 30 episodes with 15$ off with following coupon:
 
             </p>
