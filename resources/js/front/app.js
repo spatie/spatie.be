e81550b8ea2288d66b5673b988ac8b00750703ca
freekmurze <freekmurze@users.noreply.github.com>
2020-06-08 14:00:09 +0000

    PHP CS changes

--- resources/js/front/app.js
+++ resources/js/front/app.js
@@ -9,21 +9,20 @@ if ('serviceWorker' in navigator) {
 */
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(
-        function(registrations) {
-            for(let registration of registrations) { 
-                registration.unregister()
-                    .then(function() {
-                        return self.clients.matchAll();
-                    })
-                    .then(function(clients) {
-                        clients.forEach(client => {
-                            if (client.url && "navigate" in client){
-                                client.navigate(client.url);
-                            }
-                        });
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        for (let registration of registrations) {
+            registration
+                .unregister()
+                .then(function() {
+                    return self.clients.matchAll();
+                })
+                .then(function(clients) {
+                    clients.forEach(client => {
+                        if (client.url && 'navigate' in client) {
+                            client.navigate(client.url);
+                        }
                     });
-            }
+                });
         }
-    )
+    });
 }
