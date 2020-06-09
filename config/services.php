4ff19129dd73f96db3a85f744b4f35c5d832e3b0
freek <freek@spatie.be>
2020-06-09 19:08:12 +0200

    wip

--- config/services.php
+++ config/services.php
@@ -25,9 +25,9 @@ return [
     ],
 
     'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        'key' => env('SES_AWS_ACCESS_KEY_ID'),
+        'secret' => env('SES_AWS_SECRET_ACCESS_KEY'),
+        'region' => env('SES_AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
     'github' => [
