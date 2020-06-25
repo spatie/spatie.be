c53441e7ddb2dc5d6d1ad98d36e7019403bf859f
Rias <hello@rias.be>
2020-06-25 16:36:15 +0200

    Show some details

--- resources/views/front/pages/products/detail/default.blade.php
+++ resources/views/front/pages/products/detail/default.blade.php
@@ -3,7 +3,10 @@
         {{ $product->description }}
     </x-slot>
 
+    <h1>{{ $product->title }}</h1>
+    <div>{{ $product->description }}</div>
+
     @foreach($product->purchasables as $purchasable)
-            @include('front.pages.products.partials.priceCard')
+        @include('front.pages.products.partials.priceCard')
     @endforeach
 </div>
