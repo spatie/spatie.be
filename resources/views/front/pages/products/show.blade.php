93bb492e25985f1f10179ae55c71a175a9d6df0c
Rias <hello@rias.be>
2020-06-26 11:41:25 +0200

    wip

--- resources/views/front/pages/products/show.blade.php
+++ resources/views/front/pages/products/show.blade.php
@@ -6,15 +6,5 @@
         :title="$product->title"
         background="/backgrounds/home.jpg"
 >
-    @include('front.pages.products.partials.productPurchases', ['purchasesForProduct' => $purchases])
-
-    @foreach ($licenses as $license)
-        <div>
-            <span>{{ $license->purchasable->title }}</span>
-            <code class="font-mono">{{ $license->key }}</code>
-            <button class="bg-grey-lighter p-2">Renew</button>
-        </div>
-    @endforeach
-
     @includeFirst(["front.pages.products.detail.{$product->slug}", "front.pages.products.detail.default"])
 </x-page>
