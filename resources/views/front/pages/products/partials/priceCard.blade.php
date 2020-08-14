bbf5a14d6953515993d19a1e1c4d434413830901
Rias <hey@rias.be>
2020-08-14 10:04:36 +0200

    Fix prices

--- resources/views/front/pages/products/partials/priceCard.blade.php
+++ resources/views/front/pages/products/partials/priceCard.blade.php
@@ -6,26 +6,24 @@
     <h2 class="title-sm mb-6">{{ $purchasable->title }}</h2>
     
     <div class="markup-lists markup-lists-compact text-xs">
-        {{ $purchasable->description }}
+        {!! $purchasable->description !!}
     </div>
 
     <div class="mt-6 flex justify-center">
         @auth
             <x-paddle-button :url="$payLink" data-theme="none">
-                <span>Buy for&nbsp;</span>
-                <span class="leading-none">
-                    <span class="" data-id="current-currency-{{ $purchasable->id }}"></span>
-                    <span class="" data-id="current-price-{{ $purchasable->id }}"></span>
-                </span>
+                <x-button>
+                    <span>Buy for&nbsp;</span>
+                    <span data-id="current-currency-{{ $purchasable->id }}"></span>
+                    <span data-id="current-price-{{ $purchasable->id }}"></span>
+                </x-button>
             </x-paddle-button>
         @else
             <a href="{{ route('login') }}">
                 <x-button>
                     <span>Buy for&nbsp;</span>
-                    <span class="leading-none">
-                        <span class="" data-id="current-currency-{{ $purchasable->id }}"></span>
-                        <span class="" data-id="current-price-{{ $purchasable->id }}"></span>
-                    </span>
+                    <span data-id="current-currency-{{ $purchasable->id }}"></span>
+                    <span data-id="current-price-{{ $purchasable->id }}"></span>
                 </x-button>
             </a>
         @endauth
@@ -40,18 +38,19 @@
     }
 
     Paddle.Product.Prices({{ $purchasable->paddle_product_id }}, function(prices) {
-        console.log(prices);
         let priceString = prices.price.net;
-
         let indexOFirstDigitInString = indexOfFirstDigitInString(priceString);
-
         let price = priceString.substring(indexOFirstDigitInString);
         price = price.replace('.00', '');
 
         let currencySymbol = priceString.substring(0,indexOFirstDigitInString);
         currencySymbol = currencySymbol.replace('US', '');
 
-        document.querySelector('[data-id="current-currency-{{ $purchasable->id}}"]').innerHTML = currencySymbol;
-        document.querySelector('[data-id="current-price-{{ $purchasable->id }}"]').innerHTML = price;
+        document.querySelectorAll('[data-id="current-currency-{{ $purchasable->id}}"]').forEach((element) => {
+            element.innerHTML = currencySymbol;
+        });
+        document.querySelectorAll('[data-id="current-price-{{ $purchasable->id }}"]').forEach((element) => {
+            element.innerHTML = price;
+        });
     });
 </script>
