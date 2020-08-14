53dd6a40489fd08dbca28c3b9f813910dcfa65d9
Rias <hey@rias.be>
2020-08-14 12:55:28 +0200

    wip

--- app/Nova/Purchase.php
+++ app/Nova/Purchase.php
@@ -8,12 +8,9 @@ use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
-use OptimistDigital\NovaSortable\Traits\HasSortableRows;
 
 class Purchase extends Resource
 {
-    use HasSortableRows;
-
     public static $group = "Products";
 
     public static $model = EloquentPurchase::class;
