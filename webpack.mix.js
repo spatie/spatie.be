c4337752be7b5bb150643abedb45ef75e58e7097
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-19 16:39:10 +0200

    Basic tailwind CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,10 @@ const mix = require("laravel-mix");
 require('laravel-mix-purgecss');
 
 mix.js('resources/js/front/app.js', 'public/js')
-    .postCss('resources/css/front/front.css', 'public/css', [
+    // .postCss('resources/css/front/front.css', 'public/css', [
+    //     require('tailwindcss'),
+    // ])
+    .postCss('resources/css/front/test.css', 'public/css', [
         require('tailwindcss'),
     ])
     .version()
