e95bc03463131b1b80ea277ba3548599bd4f4326
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-19 16:50:59 +0200

    Try build without tailwind or CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,9 +3,10 @@ const mix = require("laravel-mix");
 require('laravel-mix-purgecss');
 
 mix.js('resources/js/front/app.js', 'public/js')
-    .postCss('resources/css/front/front.css', 'public/css', [
-        require('tailwindcss'),
-    ])
+    // .postCss('resources/css/front/front.css', 'public/css', [
+    //     require('tailwindcss'),
+    // ])
+    .postCss('resources/css/front/test.css', 'public/css')
     .version()
     .purgeCss({
         whitelistPatterns: [/active/, /grid-span/, /fancybox/, /char-/, /fill-/],
