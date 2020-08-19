84ce6b653957a517a26fdd29bed4d0756f2d2647
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-19 16:46:19 +0200

    Reset front.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,9 +3,6 @@ const mix = require("laravel-mix");
 require('laravel-mix-purgecss');
 
 mix.js('resources/js/front/app.js', 'public/js')
-    // .postCss('resources/css/front/front.css', 'public/css', [
-    //     require('tailwindcss'),
-    // ])
     .postCss('resources/css/front/front.css', 'public/css', [
         require('tailwindcss'),
     ])
