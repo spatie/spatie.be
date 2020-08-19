cef8172e2632ba63fd1e06bd54525ed0660e8b65
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-19 16:36:37 +0200

    Test with no js

--- resources/js/front/app.js
+++ resources/js/front/app.js
@@ -1,28 +1,31 @@
-import images from './images';
+// import images from './images';
+//
+// window.addEventListener('load', images);
+//
+// /*
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/service-worker.js');
+// }
+// */
+//
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.getRegistrations().then(function(registrations) {
+//         for (let registration of registrations) {
+//             registration
+//                 .unregister()
+//                 .then(function() {
+//                     return self.clients.matchAll();
+//                 })
+//                 .then(function(clients) {
+//                     clients.forEach(client => {
+//                         if (client.url && 'navigate' in client) {
+//                             client.navigate(client.url);
+//                         }
+//                     });
+//                 });
+//         }
+//     });
+// }
 
-window.addEventListener('load', images);
 
-/*
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js');
-}
-*/
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(function(registrations) {
-        for (let registration of registrations) {
-            registration
-                .unregister()
-                .then(function() {
-                    return self.clients.matchAll();
-                })
-                .then(function(clients) {
-                    clients.forEach(client => {
-                        if (client.url && 'navigate' in client) {
-                            client.navigate(client.url);
-                        }
-                    });
-                });
-        }
-    });
-}
+console.log('no js');
