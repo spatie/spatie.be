e6f576b26562cced478ce2a13ddd5afb27882c16
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-19 15:52:14 +0200

    Front end build tools, second attempt

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,18 +2,11 @@ const mix = require("laravel-mix");
 
 require('laravel-mix-purgecss');
 
-mix.version()
-    .js('resources/js/front/app.js', 'public/js')
-    .postCss('resources/css/front/front.css', 'public/css')
-
+mix.js('resources/js/front/app.js', 'public/js')
+    .postCss('resources/css/front/front.css', 'public/css', [
+        require('tailwindcss'),
+    ])
     .version()
-
-    .options({
-        postCss: [
-            require('tailwindcss'),
-        ],
-    })
-
     .purgeCss({
         whitelistPatterns: [/active/, /grid-span/, /fancybox/, /char-/, /fill-/],
     });
