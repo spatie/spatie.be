28f3d957f5566efab3c1186e796edaa59f5cc8b3
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-19 16:38:09 +0200

    Enable js again

--- resources/js/front/app.js
+++ resources/js/front/app.js
@@ -1,31 +1,28 @@
-// import images from './images';
-//
-// window.addEventListener('load', images);
-//
-// /*
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('/service-worker.js');
-// }
-// */
-//
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.getRegistrations().then(function(registrations) {
-//         for (let registration of registrations) {
-//             registration
-//                 .unregister()
-//                 .then(function() {
-//                     return self.clients.matchAll();
-//                 })
-//                 .then(function(clients) {
-//                     clients.forEach(client => {
-//                         if (client.url && 'navigate' in client) {
-//                             client.navigate(client.url);
-//                         }
-//                     });
-//                 });
-//         }
-//     });
-// }
+import images from './images';
 
+window.addEventListener('load', images);
 
-console.log('no js');
+/*
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+}
+*/
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        for (let registration of registrations) {
+            registration
+                .unregister()
+                .then(function() {
+                    return self.clients.matchAll();
+                })
+                .then(function(clients) {
+                    clients.forEach(client => {
+                        if (client.url && 'navigate' in client) {
+                            client.navigate(client.url);
+                        }
+                    });
+                });
+        }
+    });
+}
