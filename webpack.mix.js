9633ac465aa609a799540abb7fcf6e7c06cc5687
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-19 13:48:28 +0200

    Update laravel mix and build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,50 +1,17 @@
-const { mix } = require('laravel-mix');
+const mix = require("laravel-mix");
 
 require('laravel-mix-purgecss');
 
 mix.version()
-
     .js('resources/js/front/app.js', 'public/js')
     .postCss('resources/css/front/front.css', 'public/css')
 
     .version()
 
     .options({
-    // Our own set of PostCSS plugins.
         postCss: [
             require('tailwindcss'),
         ],
-
-        // CSSNext already processes our css with Autoprefixer, so we don't
-        // need mix to do it twice.
-        autoprefixer: false,
-
-        // Since we don't do any image preprocessing and write url's that are
-        // relative to the site root, we don't want the css loader to try to
-        // follow paths in `url()` functions.
-        processCssUrls: false,
-    })
-
-    .babelConfig({
-        plugins: ['syntax-dynamic-import'],
-    })
-
-    .webpackConfig({
-        output: {
-            // The public path needs to be set to the root of the site so
-            // Webpack can locate chunks at runtime.
-            publicPath: '/',
-
-            // We'll place all chunks in the `js` folder by default so we don't
-            // need to worry about ignoring them in our version control system.
-            chunkFilename: 'js/[name].js',
-        },
-
-        resolve: {
-            alias: {
-                vue$: 'vue',
-            },
-        },
     })
 
     .purgeCss({
