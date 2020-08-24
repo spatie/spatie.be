564455922f1ce73edb29ba1061d68ff9ab3b011e
Rias <hey@rias.be>
2020-08-24 15:34:49 +0200

    Add chapters

--- resources/views/front/pages/videos/partials/sidebar.blade.php
+++ resources/views/front/pages/videos/partials/sidebar.blade.php
@@ -3,40 +3,47 @@
         {{ $series->title }}
     </h2>
     <ol class="text-xs grid gap-2 links-blue markup-list-compact">
-        @forelse ($series->videos as $video)
-            <li class="{{ isset($currentVideo) && $currentVideo->id === $video->id ? "font-sans-bold" : "" }}">
-                <a class="block" href="{{ route('videos.show', [$series, $video]) }}">
-                    <span class="mr-1">{{ $video->title }}</span>
-                    
-                    @if($video->display === \App\Models\Enums\VideoDisplayEnum::FREE)
-                        <span class="hidden tag tag-green">Free</span>
-                    @endif
+        @forelse ($series->videos->groupBy('chapter') as $chapter => $videosPerChapter)
+            @if ($chapter)
+                <h3 class="title-sm text-xs">
+                    {{ $chapter }}
+                </h3>
+            @endif
+            @foreach($videosPerChapter as $video)
+                <li class="{{ isset($currentVideo) && $currentVideo->id === $video->id ? "font-sans-bold" : "" }}">
+                    <a class="block" href="{{ route('videos.show', [$series, $video]) }}">
+                        <span class="mr-1">{{ $video->title }}</span>
 
-                    @if($video->display === \App\Models\Enums\VideoDisplayEnum::SPONSORS)
-                        <span title="Exclusive for sponsors" class="w-4 h-4 inline-flex items-center justify-center bg-pink-lightest rounded-full">
-                            <span style="font-size: .6rem" class="icon text-pink">
-                                {{ svg('icons/fas-heart') }}
-                            </span>
-                        </span>
-                    @endif
+                        @if($video->display === \App\Models\Enums\VideoDisplayEnum::FREE)
+                            <span class="hidden tag tag-green">Free</span>
+                        @endif
 
-                    @if($video->display === \App\Models\Enums\VideoDisplayEnum::LICENSE &&  ! $video->canBeSeenByCurrentUser() )
-                        <span title="Part of course" class="w-4 h-4 inline-flex items-center justify-center bg-green-lightest rounded-full">
-                            <span style="font-size: .6rem; top: -.1rem" class="icon text-green">
-                                {{ svg('icons/fas-lock-alt') }}
+                        @if($video->display === \App\Models\Enums\VideoDisplayEnum::SPONSORS)
+                            <span title="Exclusive for sponsors" class="w-4 h-4 inline-flex items-center justify-center bg-pink-lightest rounded-full">
+                                <span style="font-size: .6rem" class="icon text-pink">
+                                    {{ svg('icons/fas-heart') }}
+                                </span>
                             </span>
-                        </span>
-                    @endif
+                        @endif
 
-                    @if($video->hasBeenCompletedByCurrentUser())
-                        <span title="Completed" style="left: calc(-1.5em - 1.5rem)" class="absolute top-0 w-4 h-4 inline-flex items-center justify-center bg-green rounded-full">
-                            <span style="font-size: 0.75rem" class="text-white">
-                                ✓
+                        @if($video->display === \App\Models\Enums\VideoDisplayEnum::LICENSE &&  ! $video->canBeSeenByCurrentUser() )
+                            <span title="Part of course" class="w-4 h-4 inline-flex items-center justify-center bg-green-lightest rounded-full">
+                                <span style="font-size: .6rem; top: -.1rem" class="icon text-green">
+                                    {{ svg('icons/fas-lock-alt') }}
+                                </span>
                             </span>
-                        </span>
-                    @endif
-                </a>
-            </li>
+                        @endif
+
+                        @if($video->hasBeenCompletedByCurrentUser())
+                            <span title="Completed" style="left: calc(-1.5em - 1.5rem)" class="absolute top-0 w-4 h-4 inline-flex items-center justify-center bg-green rounded-full">
+                                <span style="font-size: 0.75rem" class="text-white">
+                                    ✓
+                                </span>
+                            </span>
+                        @endif
+                    </a>
+                </li>
+            @endforeach
         @empty
             <li>No videos yet! Stay tuned...</li>
         @endforelse
