10f44406e63dd476aa70c704334f9e306bc886df
AlexVanderbist <AlexVanderbist@users.noreply.github.com>
2020-08-25 14:12:20 +0000

    PHP CS changes

--- app/Console/Commands/ImportDocsFromRepositoriesCommand.php
+++ app/Console/Commands/ImportDocsFromRepositoriesCommand.php
@@ -2,13 +2,12 @@
 
 namespace App\Console\Commands;
 
-use App\Docs\Docs;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use React\ChildProcess\Process;
 use React\EventLoop\Factory;
-use Spatie\Sheets\Sheets;
 use function React\Promise\all;
+use Spatie\Sheets\Sheets;
 use function WyriHaximus\React\childProcessPromise;
 
 class ImportDocsFromRepositoriesCommand extends Command
