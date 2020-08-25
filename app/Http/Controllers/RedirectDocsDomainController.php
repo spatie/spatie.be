f7314f9b937970fe701dc91c293e5334222129f5
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-25 16:15:14 +0200

    Strip trailing slash on docs redirect

--- app/Http/Controllers/RedirectDocsDomainController.php
+++ app/Http/Controllers/RedirectDocsDomainController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 
 class RedirectDocsDomainController
 {
     public function __invoke(string $url = '')
     {
+        $url = Str::beforeLast($url, '/');
+
         return Redirect::to("https://spatie.be/docs/{$url}", 301);
     }
 }
