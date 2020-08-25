6a735c27f946d513680a6d0a9c5b664d2a69c650
freek <freek@spatie.be>
2020-08-25 11:18:32 +0200

    wip

--- resources/views/front/pages/open-source/partials/packages-intro.blade.php
+++ resources/views/front/pages/open-source/partials/packages-intro.blade.php
@@ -3,9 +3,11 @@
         <div class="markup links-underline links-blue">
             <p class="text-lg">
                 For most of our projects we use Laravel. We are actively contributing to this awesome ecosystem.
-                <br/><br/>
+                <p>
+            <p class="text-lg">
                 Whenever we see functionality in our Laravel, PHP and JavaScript projects that could be useful to others, we extract it to a package.
-                <br><br/>
+            </p>
+            <p class="text-lg">
                 Pretty sure there's something here for your projects!
             </p>
         </div>
