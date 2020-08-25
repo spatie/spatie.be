220443fdfd76159536c189e6ec8fc8c87077a688
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-25 10:41:47 +0200

    Update postcard regenerate code

--- app/Console/Commands/RegeneratePostcardResponsiveImagesCommand.php
+++ app/Console/Commands/RegeneratePostcardResponsiveImagesCommand.php
@@ -9,12 +9,17 @@ use Spatie\MediaLibrary\ResponsiveImages\Jobs\GenerateResponsiveImagesJob;
 
 class RegeneratePostcardResponsiveImagesCommand extends Command
 {
-    protected $signature = 'postcards:regenerate';
+    protected $signature = 'postcards:regenerate {--id}';
 
     protected $description = 'Regenerates the responsive images for all the postcards';
 
     public function handle()
     {
+        if ($id = $this->option('id')) {
+            dispatch(new GenerateResponsiveImagesJob(Media::find($id)));
+            return;
+        }
+
         Postcard::each(function (Postcard $postcard) {
             $postcard->getMedia()->each(fn (Media $media) => dispatch(new GenerateResponsiveImagesJob($media)));
         });
