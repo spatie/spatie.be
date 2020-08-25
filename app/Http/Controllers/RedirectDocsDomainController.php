cb814547225ae43cad5c36ff0d9d83f5bccbf5de
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-25 15:10:46 +0200

    Fix redirect temp

--- app/Http/Controllers/RedirectDocsDomainController.php
+++ app/Http/Controllers/RedirectDocsDomainController.php
@@ -6,6 +6,6 @@ class RedirectDocsDomainController
 {
     public function __invoke(string $url)
     {
-        return redirect("/docs/{$url}", 301);
+        return redirect("https://spatie.be/docs/{$url}", 301);
     }
 }
