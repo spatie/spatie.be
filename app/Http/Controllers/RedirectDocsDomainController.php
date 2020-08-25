edb9dc64e679d924ae2d46c8dbc8d65ad121034d
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-25 15:13:22 +0200

    Default to empty string for docs redirect

--- app/Http/Controllers/RedirectDocsDomainController.php
+++ app/Http/Controllers/RedirectDocsDomainController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 class RedirectDocsDomainController
 {
-    public function __invoke(string $url)
+    public function __invoke(string $url = '')
     {
         return redirect("https://spatie.be/docs/{$url}", 301);
     }
