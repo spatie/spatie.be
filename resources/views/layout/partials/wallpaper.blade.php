9d5b3ad2f7d61eeaa9bcf72c0d32b464425f8441
Rias <hey@rias.be>
2020-08-26 09:31:07 +0200

    Fix wallpaper issue

--- resources/views/layout/partials/wallpaper.blade.php
+++ resources/views/layout/partials/wallpaper.blade.php
@@ -1,4 +1,4 @@
-@if($background && $image = image($background))
+@if(isset($background) && $background && $image = image($background))
     <div class="wallpaper opacity-50 | md:opacity-100">
         {{-- wallpaper is always 100vw, so we don't need pragmatic srcset sizes --}}
         <img srcset="{{ $image->getSrcset() }}" src="{{ $image->getUrl() }}" width="2400" sizes="100vw" alt="">
