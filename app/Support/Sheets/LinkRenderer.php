1ad058876cef43bd3f34efdb241de7dfacb08a0c
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-08-31 13:26:13 +0200

    Add docs prefix to links

---
+++ app/Support/Sheets/LinkRenderer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Support\Sheets;
+
+use Illuminate\Support\Str;
+use League\CommonMark\ElementRendererInterface;
+use League\CommonMark\HtmlElement;
+use League\CommonMark\Inline\Element\AbstractInline;
+use League\CommonMark\Inline\Element\Link;
+use League\CommonMark\Inline\Renderer\InlineRendererInterface;
+use League\CommonMark\Util\ConfigurationAwareInterface;
+use League\CommonMark\Util\ConfigurationInterface;
+use League\CommonMark\Util\RegexHelper;
+
+class LinkRenderer implements InlineRendererInterface, ConfigurationAwareInterface
+{
+    /** @var ConfigurationInterface */
+    protected $config;
+
+    /**
+     * @param Link $inline
+     * @param ElementRendererInterface $htmlRenderer
+     *
+     * @return HtmlElement
+     */
+    public function render(AbstractInline $inline, ElementRendererInterface $htmlRenderer)
+    {
+        if (! ($inline instanceof Link)) {
+            throw new \InvalidArgumentException('Incompatible inline type: ' . \get_class($inline));
+        }
+
+        $attrs = $inline->getData('attributes', []);
+
+        $forbidUnsafeLinks = ! $this->config->get('allow_unsafe_links');
+        if (! ($forbidUnsafeLinks && RegexHelper::isLinkPotentiallyUnsafe($inline->getUrl()))) {
+            $attrs['href'] = $inline->getUrl();
+        }
+
+        if (Str::startsWith($attrs['href'], '/')) {
+            $attrs['href'] = '/docs'.$attrs['href'];
+        }
+
+        if (isset($inline->data['title'])) {
+            $attrs['title'] = $inline->data['title'];
+        }
+
+        if (isset($attrs['target']) && $attrs['target'] === '_blank' && ! isset($attrs['rel'])) {
+            $attrs['rel'] = 'noopener noreferrer';
+        }
+
+        return new HtmlElement('a', $attrs, $htmlRenderer->renderInlines($inline->children()));
+    }
+
+    public function setConfiguration(ConfigurationInterface $configuration)
+    {
+        $this->config = $configuration;
+    }
+}
