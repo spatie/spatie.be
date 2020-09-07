d02ea6f84b49f6efc1f8050eb3922bee5fd23477
Willem Van Bockstal <willem@spatie.be>
2020-09-07 15:52:38 +0200

    Add docs banner

---
+++ resources/views/front/pages/docs/banners/medialibrary.blade.php
@@ -0,0 +1,20 @@
+<aside class="z-20 fixed flex bottom-0 w-full p-2">
+    <a class="mx-auto" href="https://medialibrary.pro">
+        <div class="bg-orange text-white flex flex-col items-end sm:flex-row sm:items-center justify-center rounded p-2 sm:p-4 shadow-light text-xs sm:text-sm">
+            <div class="flex items-center">
+                <div class="mr-2 text-lg icon bg-black bg-opacity-25 text-white rounded-full w-8 flex items-center justify-center h-8">
+                    {{ svg('icons/far-image') }}
+                </div>
+                <div>
+                    <div>
+                        <span class="font-semibold">Medialibrary.pro</span>
+                        – UI components for the Media Library
+                    </div>
+                </div>
+            </div>
+            <button class="mt-2 md:mt-0 ml-4 px-2 py-1 rounded text-orange bg-white uppercase tracking-wide font-semibold">
+                Learn&nbsp;more
+            </button>
+        </div>
+    </a>
+</aside>
--- resources/views/front/pages/docs/partials/navigation.blade.php
+++ resources/views/front/pages/docs/partials/navigation.blade.php
@@ -21,29 +21,24 @@
     </div>
 
     <div class="sticky top-0 pt-4 ">
-    <input type="search" class="text-xs form-input w-full h-8 py-0 px-2 mb-8" id="algolia-search" placeholder="Search…">
-    <ol class="text-xs grid gap-2 links-blue">
-        @foreach($navigation as $key => $section)
-            @if ($key !== '_root')
-                <h2 class="title-sm text-sm">{{ $section['_index']['title'] }}</h2>
-            @endif
+        <input type="search" class="text-xs form-input w-full h-8 py-0 px-2 mb-8" id="algolia-search" placeholder="Search…">
+        <ol class="text-xs grid gap-2 links-blue">
+            @foreach($navigation as $key => $section)
+                @if ($key !== '_root')
+                    <h2 class="title-sm text-sm">{{ $section['_index']['title'] }}</h2>
+                @endif
 
-            <ul class="mb-6 space-y-1 links-blue @if($key !== '_root') pl-3 border-l-2 border-gray-lighter border-opacity-75 @endif">
-                @foreach($section['pages'] as $navItem)
-                    <li class="leading-snug">
-                        <a href="{{ $navItem->url }}" class="@if($page->slug === $navItem->slug) font-bold @endif">
-                            {{ $navItem->title }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        @endforeach
-    </ol>
-
-    <a href="https://laravelpackage.training?utm_source=docs.spatie.be"><img
-                class="mb-8"
-                src="https://d33wubrfki0l68.cloudfront.net/dc2ab82b48c72af8e3fa738348653bf0b08a011c/6eae2/images/package-training.jpg"></a>
-
+                <ul class="mb-6 space-y-1 links-blue @if($key !== '_root') pl-3 border-l-2 border-gray-lighter border-opacity-75 @endif">
+                    @foreach($section['pages'] as $navItem)
+                        <li class="leading-snug">
+                            <a href="{{ $navItem->url }}" class="@if($page->slug === $navItem->slug) font-bold @endif">
+                                {{ $navItem->title }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            @endforeach
+        </ol>
     </div>
 
 </nav>
