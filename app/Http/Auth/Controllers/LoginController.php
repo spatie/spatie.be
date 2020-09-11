fc68578f3456485cd654f77b660077562e8a9cfd
rubenvanassche <rubenvanassche@users.noreply.github.com>
2020-09-11 07:36:29 +0000

    PHP CS changes

--- app/Http/Auth/Controllers/LoginController.php
+++ app/Http/Auth/Controllers/LoginController.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
