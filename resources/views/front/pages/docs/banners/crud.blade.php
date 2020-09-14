9629ae5efab0b7d4161d68e2d2e1c997ecba44c1
Willem Van Bockstal <willem@spatie.be>
2020-09-14 09:02:13 +0200

    banners

--- resources/views/front/pages/docs/banners/crud.blade.php
+++ resources/views/front/pages/docs/banners/crud.blade.php
@@ -1,5 +1,5 @@
 <aside class="z-20 fixed flex bottom-0 w-full p-2">
-    <a class="mx-auto" href="https://laravel-beyond-crud.com">
+    <div class="mx-auto">
         <div class="bg-purple text-white flex flex-col items-end sm:flex-row sm:items-center justify-center rounded p-2 sm:p-4 shadow-light text-xs sm:text-sm">
             <div class="flex items-center">
                 <div class="mr-2 text-lg icon bg-black bg-opacity-25 text-white rounded-full w-8 flex items-center justify-center h-8">
@@ -12,9 +12,12 @@
                     </div>
                 </div>
             </div>
-            <button class="mt-2 md:mt-0 ml-4 px-2 py-1 rounded text-purple bg-white uppercase tracking-wide font-semibold">
-                Learn&nbsp;more
-            </button>
+            <a href="https://laravel-beyond-crud.com">
+                <button class="mt-2 md:mt-0 ml-4 px-2 py-1 rounded text-purple bg-white uppercase tracking-wide font-semibold">
+                    Learn&nbsp;more
+                </button>
+            </a>
+            <button class="absolute top-0 right-0 mr-1 opacity-50" onClick="this.parentElement.remove(); r">&times;</button>
         </div>
-    </a>
+    </div>
 </aside>
