874b88dd1fea7e8cd36be46049eeee67b99cde1a
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-09-22 17:01:33 +0200

    Prepare new goal tracking for purchasables

--- resources/views/front/pages/products/show.blade.php
+++ resources/views/front/pages/products/show.blade.php
@@ -6,6 +6,5 @@
         :title="$product->title"
         background="/backgrounds/product-blur.jpg"
 >
-
     @includeFirst(["front.pages.products.detail.{$product->slug}", "front.pages.products.detail.default"])
 </x-page>
--- resources/views/layout/partials/analytics.blade.php
+++ resources/views/layout/partials/analytics.blade.php
@@ -1,4 +1,4 @@
-@if(app()->environment('production'))
+@if(app()->environment('production') || true)
     <script type="text/javascript">
         var _paq = _paq || [];
 
@@ -12,5 +12,9 @@
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
             g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
         })();
+
+        @if(session()->has('completed_goal_id'))
+            _paq.push(['trackGoal', {{session()->get('completed_goal_id')}}, {{ session()->get('completed_goal_earnings', 0) }}]);
+        @endif
     </script>
 @endif
