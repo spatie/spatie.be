c79c4cb2051e6d361b52760b53a0417cf50a8d3e
freek <freek@spatie.be>
2020-10-01 14:30:50 +0200

    wip

--- app/Notifications/WelcomeFrontLinePhpWaitingListNotification.php
+++ app/Notifications/WelcomeFrontLinePhpWaitingListNotification.php
@@ -22,7 +22,8 @@ class WelcomeFrontLinePhpWaitingListNotification extends Notification
             ->subject("You are now subscribed to the Front Line PHP waiting list")
             ->greeting('Hi!')
             ->line("Thank you for subscribing to the Front line PHP waiting list. We are very exciting about PHP 8 and can't wait to share our insights and knowledge with you.")
-            ->line('We would like to offer you this coupon code, that grants your a 25% discount on all products in our store: WAITING-FOR-FRONT-LINE_PHP')
-            ->action('Visit our little show', route('products.index'));
+            ->line('We would like to offer you this coupon code, that grants your a 25% discount on all products in our store:')
+            ->line('WAITING-FOR-FRONT-LINE_PHP')
+            ->action('Visit our store', route('products.index'));
     }
 }
