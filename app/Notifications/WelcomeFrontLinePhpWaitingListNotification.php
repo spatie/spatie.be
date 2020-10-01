33f8b61095407a57a58b226895a467a57e062c22
freek <freek@spatie.be>
2020-10-01 14:15:49 +0200

    wip

--- app/Notifications/WelcomeFrontLinePhpWaitingListNotification.php
+++ app/Notifications/WelcomeFrontLinePhpWaitingListNotification.php
@@ -18,12 +18,11 @@ class WelcomeFrontLinePhpWaitingListNotification extends Notification
 
     public function toMail(Subscriber $notifiable): MailMessage
     {
-        info('sending welcome mail');
-
         return (new MailMessage)
-            ->subject("Welcome")
+            ->subject("You are now subscribed to the Front Line PHP waiting list")
             ->greeting('Hi!')
-            ->action('Visit our little show', route('products.index'))
-            ->line("Thank you!");
+            ->line("Thank you for subscribing to the Front line PHP waiting list. We are very exciting about PHP 8 and can't wait to share our insights and knowledge with you.")
+            ->line('We would like to offer you this coupon code, that grants your a 25% discount on all products in our store: WAITING-FOR-FRONT-LINE_PHP')
+            ->action('Visit our little show', route('products.index'));
     }
 }
