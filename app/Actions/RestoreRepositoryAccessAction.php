0766c22353773ae753220871bd60c4239159d117
freek <freek@spatie.be>
2020-10-10 16:16:20 +0200

    wip

--- app/Actions/RestoreRepositoryAccessAction.php
+++ app/Actions/RestoreRepositoryAccessAction.php
@@ -19,15 +19,9 @@ class RestoreRepositoryAccessAction
     {
         $user->purchases
             ->where('has_repository_access', false)
+            ->filter(fn(Purchase $purchase) => $purchase->purchasable->repository_access)
+            ->reject(fn(Purchase $purchase) => $purchase->license && $purchase->license->isExpired())
             ->each(function (Purchase $purchase) use ($user) {
-                if (! $purchase->purchasable->repository_access) {
-                    return;
-                }
-
-                if ($purchase->license && $purchase->license->isExpired()) {
-                    return;
-                }
-
                 $this->gitHubApi->inviteToRepo(
                     $user->github_username,
                     $purchase->purchasable->repository_access
