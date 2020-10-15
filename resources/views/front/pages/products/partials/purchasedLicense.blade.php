6c561f42fd2dcf96329b60dbf884754f0bc0ba58
Willem Van Bockstal <willem@spatie.be>
2020-10-15 11:39:32 +0200

    Getting started message

--- resources/views/front/pages/products/partials/purchasedLicense.blade.php
+++ resources/views/front/pages/products/partials/purchasedLicense.blade.php
@@ -1,44 +1,50 @@
 <div class="cells grid-cols-2">
     <div class="cell-l">
-        <code class="break-all font-mono text-blue bg-blue-lightest bg-opacity-25 px-2 py-1 rounded-sm">{{ $license->key }}</code>
+        
+        <div class="mt-2 flex justify-start">
+            @if ($license->purchasable->getting_started_url)
+                <a class="link-blue link-underline" href="{{ $license->purchasable->getting_started_url }}">
+                    Getting started
+                </a>
+            @endif
+
+            @if ($license->purchasable->series->count())
+                <a class="link-blue link-underline" href="{{ route('series.show', $license->purchasable->series->first()) }}">
+                    Videos
+                </a>
+            @endif
+
+            @if ($license->purchasable->repository_access)
+                @if ($license->hasRepositoryAccess())
+                    <a class="link-blue link-underline" href="https://github.com/{{ $license->purchasable->repository_access }}">
+                        Repository
+                    </a>
+                @else
+                    <a class="link-blue link-underline" href="{{ route('github-login') }}">
+                        Connect to GitHub to access repo
+                    </a>
+                @endif
+            @endif
+        </div>
+
+        <p>
+            <div class="text-xs text-gray">License key:</div>
+            <code class="break-all font-mono text-blue bg-blue-lightest bg-opacity-25 px-2 py-1 rounded-sm">{{ $license->key }}</code>
+        </p>
+
         <div class="flex items-center text-xs text-gray">
             <livewire:domain :license="$license" />
 
             @if ($license->isExpired())
-            <span class="text-pink-dark">Expired since {{ $license->expires_at->format('Y-m-d') }}</span>
+                <span class="text-pink-dark">Expired since {{ $license->expires_at->format('Y-m-d') }}</span>
             @else
-            <span>Expires on {{ $license->expires_at->format('Y-m-d') }}</span>
+                <span>Expires on {{ $license->expires_at->format('Y-m-d') }}</span>
             @endif
         </div>
 
-        <div class="mt-2 grid grid-flow-col gap-4 justify-start">
-            @if ($license->purchasable->getting_started_url)
-            <a class="link-blue link-underline" href="{{ $license->purchasable->getting_started_url }}">
-                Getting started
-            </a>
-            @endif
-
-            @if ($license->purchasable->series->count())
-            <a class="link-blue link-underline" href="{{ route('series.show', $license->purchasable->series->first()) }}">
-                Videos
-            </a>
-            @endif
-
-            @if ($license->purchasable->repository_access)
-            @if ($license->hasRepositoryAccess())
-            <a class="link-blue link-underline" href="https://github.com/{{ $license->purchasable->repository_access }}">
-                Repository
-            </a>
-            @else
-            <a class="link-blue link-underline" href="{{ route('github-login') }}">
-                Connect to GitHub to access repo
-            </a>
-            @endif
-            @endif
-        </div>
     </div>
 
-    <span class="cell-r grid gap-4 justify-start md:justify-end">
+    <span class="cell-r ml-auto">
         @if ($license->purchasable->renewalPurchasable)
         <x-paddle-button :url="auth()->user()->getPayLinkForProductId($license->purchasable->renewalPurchasable->paddle_product_id)" data-theme="none">
             <x-button>
