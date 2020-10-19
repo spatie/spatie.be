72ca71080425ea02ee4642ab96020a4707a30d49
freek <freek@spatie.be>
2020-10-19 08:45:42 +0200

    let guest middleware redirect to the profile page

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect('/');
+                return redirect(route('profile'));
             }
         }
 
