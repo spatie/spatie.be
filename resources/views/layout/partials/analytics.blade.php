6b7c130b0a011a34cbd7a5737bfc091b285c96f9
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-10-28 17:35:13 +0100

    Add product title to analytics tracking

--- resources/views/layout/partials/analytics.blade.php
+++ resources/views/layout/partials/analytics.blade.php
@@ -28,7 +28,7 @@
                 "items": [
                     {
                         "id": "{{ $purchasable->id }}",
-                        "name": "{{ $purchasable->title }}",
+                        "name": "{{ $purchasable->product->title }} | {{ $purchasable->title }}",
                         "quantity": 1,
                         "price": {{ $purchasable->getAverageEarnings() }}
                     }
