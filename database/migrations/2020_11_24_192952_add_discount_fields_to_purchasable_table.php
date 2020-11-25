2bac42130d6a30913d09d684a4ce1cb7663e808e
freek <freek@spatie.be>
2020-11-25 20:33:28 +0100

    wip

--- database/migrations/2020_11_24_192952_add_discount_fields_to_purchasable_table.php
+++ database/migrations/2020_11_24_192952_add_discount_fields_to_purchasable_table.php
@@ -10,13 +10,13 @@ class AddDiscountFieldsToPurchasableTable extends Migration
     {
         Schema::table('purchasables', function (Blueprint $table) {
             $table->integer('price_without_discount_in_usd_cents');
-            $table->integer('discount_name')->nullable();
+            $table->string('discount_name')->nullable();
             $table->integer('discount_percentage')->nullable();
             $table->timestamp('discount_starts_at')->nullable();
             $table->timestamp('discount_expires_at')->nullable();
         });
 
-        Schema::table('products', function(Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('coupon_code');
             $table->dropColumn('coupon_percentage');
             $table->dropColumn('coupon_valid_from');
