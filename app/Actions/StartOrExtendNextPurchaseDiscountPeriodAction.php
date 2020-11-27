da24ab6543182d04ef267af80658e3b741c0dcdd
freek <freek@spatie.be>
2020-11-27 11:55:40 +0100

    wip

--- app/Actions/StartOrExtendNextPurchaseDiscountPeriodAction.php
+++ app/Actions/StartOrExtendNextPurchaseDiscountPeriodAction.php
@@ -9,7 +9,7 @@ class StartOrExtendNextPurchaseDiscountPeriodAction
     public function execute(User $user)
     {
         $user->update([
-            'next_purchase_discount_period_ends_at', now()->addHours(24),
+            'next_purchase_discount_period_ends_at' => now()->addHours(24),
         ]);
     }
 }
