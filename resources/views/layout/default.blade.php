9c354fbbd067fce7642113a6cc364ee02df7597a
freek <freek@spatie.be>
2020-11-30 23:32:06 +0100

    wip

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -21,7 +21,9 @@
     <script>/* Empty script tag because Firefox has a FOUC */</script>
     @include('layout.partials.wallpaper')
 
+    {{--
     @include('layout.partials.cta')
+    --}}
 
     @include('layout.partials.header')
     @include('layout.partials.flash')
