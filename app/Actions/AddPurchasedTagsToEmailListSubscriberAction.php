916d008370453f8a7fa98d8bf117641942cee82f
freek <freek@spatie.be>
2020-11-30 09:48:11 +0100

    wip

---
+++ app/Actions/AddPurchasedTagsToEmailListSubscriberAction.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Purchase;
+use App\Models\Subscriber;
+use Illuminate\Support\Str;
+use Spatie\Mailcoach\Exceptions\CouldNotSubscribe;
+use Spatie\Mailcoach\Models\EmailList;
+
+class AddPurchasedTagsToEmailListSubscriberAction
+{
+    public function execute(Purchase $purchase)
+    {
+        if (empty($purchase->user->email)) {
+            return;
+        }
+
+        $emailList = EmailList::firstWhere('name', 'Spatie');
+
+        try {
+            $subscriber = $this->findOrCreateSubscriber($purchase->user->email, $emailList);
+        } catch (CouldNotSubscribe $exception) {
+            report($exception);
+            return;
+        }
+
+        $tagNames = $this->getTagNames($purchase);
+
+        $subscriber->addTags($tagNames);
+    }
+
+    protected function findOrCreateSubscriber(string $email, EmailList $emailList): Subscriber
+    {
+        if ($subscriber = Subscriber::findForEmail($email, $emailList)) {
+            return $subscriber;
+        }
+
+        $subscriber = Subscriber::createWithEmail($email)
+            ->skipConfirmation()
+            ->doNotSendWelcomeMail()
+            ->subscribeTo($emailList);
+
+        return Subscriber::find($subscriber->id);
+    }
+
+    protected function getTagNames(Purchase $purchase): array
+    {
+        $productName = $purchase->purchasable->product->title;
+        $purchasableName = $purchase->purchasable->title;
+
+        return [
+            "purchased-product-" . Str::slug($productName),
+            "purchased-purchasable-" . Str::slug($purchasableName),
+        ];
+    }
+}
---
+++ tests/Actions/AddPurchasedTagsToEmailListSubscriberActionTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Actions;
+
+use App\Actions\AddPurchasedTagsToEmailListSubscriberAction;
+use App\Models\Purchase;
+use App\Models\Subscriber;
+use Illuminate\Support\Str;
+use Spatie\Mailcoach\Models\EmailList;
+use Tests\TestCase;
+
+class AddPurchasedTagsToEmailListSubscriberActionTest extends TestCase
+{
+    /** @test */
+    public function it_will_add_tags_for_the_purchasable_on_the_mailing_list()
+    {
+        /** @var EmailList $emailList */
+        $emailList = EmailList::create(['name' => 'Spatie']);
+
+        $purchase = Purchase::factory()->create();
+
+        (new AddPurchasedTagsToEmailListSubscriberAction())->execute($purchase);
+
+        $this->assertTrue($emailList->isSubscribed($purchase->user->email));
+
+        $subscriber = Subscriber::findForEmail($purchase->user->email, $emailList);
+
+        $tagNames = $subscriber->tags->pluck('name')->toArray();
+
+        $this->assertEquals([
+            "purchased-product-" . Str::slug($purchase->purchasable->product->title),
+            "purchased-purchasable-" . Str::slug($purchase->purchasable->title),
+        ], $tagNames);
+    }
+
+    /** @test */
+    public function the_AddPurchasedTagsToEmailListSubscriberAction_is_idempotent()
+    {
+        /** @var EmailList $emailList */
+        $emailList = EmailList::create(['name' => 'Spatie']);
+
+        $purchase = Purchase::factory()->create();
+
+        (new AddPurchasedTagsToEmailListSubscriberAction())->execute($purchase);
+        (new AddPurchasedTagsToEmailListSubscriberAction())->execute($purchase);
+
+        $this->assertCount(1, $emailList->subscribers);
+
+        $this->assertCount(2, $emailList->subscribers->first()->tags);
+    }
+}
