d4dd9b2c72b19792bc3ab09db67e27d9a84d179e
freek <freek@spatie.be>
2020-12-01 09:20:38 +0100

    wip

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -92,7 +92,7 @@ class Product extends Model implements HasMedia, Sortable
 
     public function purchasableWithDiscount(): ?Purchasable
     {
-        return collect($this->purchasables)
+        return $this->purchasables
             ->filter(fn(Purchasable $purchasable) => $purchasable->hasActiveDiscount())
             ->first();
     }
