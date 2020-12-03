95c8a5744c2acf5e2dc52f08a5d1f31fb7831515
freek <freek@spatie.be>
2020-12-03 15:04:01 +0100

    wip

--- app/Models/Activation.php
+++ app/Models/Activation.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
-use Spatie\Crypto\PrivateKey;
+use Spatie\Crypto\Rsa\PrivateKey;
 
 class Activation extends Model
 {
