e8842eeaee93df71c095888e9aeb78caa1600cec
freek <freek@spatie.be>
2021-01-05 09:10:36 +0100

    wip

---
+++ app/Http/Middleware/ProtectDocsOfUnreleasedPackages.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ProtectDocsOfUnreleasedPackages
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (! Str::startsWith($request->url(), '/docs/ray')) {
+            return $next($request);
+        }
+
+        if (! $user = auth()->user()) {
+            abort(403);
+        }
+
+        if (! $user->hasAccessToUnreleasedPurchasables()) {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
