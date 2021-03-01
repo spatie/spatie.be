3c2467edd0e0e9ec2b3dd344f0f9e339e0f23326
freek <freek@spatie.be>
2021-03-01 13:32:40 +0100

    wip

--- resources/views/front/pages/docs/show.blade.php
+++ resources/views/front/pages/docs/show.blade.php
@@ -38,7 +38,7 @@
         </div>
     </section>
 
-    {{-- @include('front.pages.docs.banners.randomBanner', ['repository' => $repository]) --}}
+     @include('front.pages.docs.banners.randomBanner', ['repository' => $repository])
 
     <script src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
 
