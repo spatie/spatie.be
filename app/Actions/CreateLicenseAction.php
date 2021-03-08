f418f754e1fb70316c364485ad3d059a0bcab192
freek <freek@spatie.be>
2021-03-08 14:51:40 +0100

    wip

--- app/Actions/CreateLicenseAction.php
+++ app/Actions/CreateLicenseAction.php
@@ -13,6 +13,7 @@ class CreateLicenseAction
 {
     public function execute(User $user, ?Purchase $purchase = null, ?Purchasable $purchasable = null): License
     {
+        ray('creating license');
         return License::create([
             'key' => Str::random(64),
             'user_id' => $user->id,
--- app/Actions/HandlePurchaseAction.php
+++ app/Actions/HandlePurchaseAction.php
@@ -45,6 +45,8 @@ class HandlePurchaseAction
         PaddlePayload $paddlePayload,
         ?Referrer $referrer = null
     ): Purchase {
+        ray('handling purchase', $purchasable);
+
         $purchase = $this->createPurchase($user, $purchasable, $paddlePayload);
 
         $purchase = $this->handlePurchaseLicensingAction->execute($purchase);
