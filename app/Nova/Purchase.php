4abeb66df35fecc15292c583b369f050d32bdfb8
Rias <hey@rias.be>
2021-03-23 09:14:24 +0100

    Fix Nova relations

--- app/Nova/Purchase.php
+++ app/Nova/Purchase.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 
@@ -33,7 +34,7 @@ class Purchase extends Resource
             Boolean::make('Has repository access')->readonly(),
 
             BelongsTo::make('Purchasable'),
-            BelongsTo::make('License')->hideFromIndex()->nullable(),
+            HasMany::make('Licenses')->hideFromIndex()->nullable(),
             BelongsTo::make('User'),
             BelongsTo::make('Receipt')->nullable()->hideFromIndex(),
 
