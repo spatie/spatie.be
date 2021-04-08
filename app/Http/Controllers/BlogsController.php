6e3c107b2c0e37a85f6c5213ddb4f0835a6497c0
freek <freek@spatie.be>
2021-04-08 19:45:30 +0200

    wip

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -8,8 +8,6 @@ class BlogsController
 {
     public function index()
     {
-        ray()->showQueries();
-
         $insights = Insight::orderBy('created_at', 'DESC')->paginate(10);
 
         return view('front.pages.blog.index', ['posts' => $insights]);
