a88cd8bce62bedb3327cdc47fb197a813f2e7b60
Niels <niels@spatie.be>
2021-05-04 13:16:08 +0200

    make github username null if not found on github anymore

--- app/Console/Commands/RevokeRepositoryAccessForExpiredLicensesCommand.php
+++ app/Console/Commands/RevokeRepositoryAccessForExpiredLicensesCommand.php
@@ -3,15 +3,23 @@
 namespace App\Console\Commands;
 
 use App\Models\License;
+use App\Models\User;
 use App\Services\GitHub\GitHubApi;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Log;
+use RuntimeException;
 
 class RevokeRepositoryAccessForExpiredLicensesCommand extends Command
 {
     public $signature = 'revoke-repository-access-for-expired-licenses';
 
-    public function handle(GitHubApi $gitHubApi)
+    public function __construct(private Log $log)
+    {
+        parent::__construct();
+    }
+
+    public function handle(GitHubApi $gitHubApi): void
     {
         $this->info('Revoking access to repositories for expired licenses...');
 
@@ -20,19 +28,43 @@ class RevokeRepositoryAccessForExpiredLicensesCommand extends Command
             ->whereExpired()
             ->cursor()
             ->each(function (License $license) use ($gitHubApi) {
-                if ($license->purchase->user->licenses()->whereNotExpired()->where('purchasable_id', $license->purchasable_id)->exists()) {
+                if ($license->purchase->user->licenses()
+                    ->whereNotExpired()
+                    ->where('purchasable_id', $license->purchasable_id)
+                    ->exists()) {
                     // User has another license for this repo
                     return;
                 }
 
-                $gitHubApi->revokeAccessToRepo(
-                    $license->purchase->user->github_username,
-                    $license->purchase->purchasable->repository_access
-                );
+                try {
+                    $gitHubApi->revokeAccessToRepo(
+                        $license->purchase->user->github_username,
+                        $license->purchase->purchasable->repository_access
+                    );
+                } catch (RuntimeException $e) {
+                    if ($e->getMessage() !== "Not Found") {
+                        $this->log::alert(
+                            "We could not revoke access for {$license->purchase->user->github_username}
+                             to {$license->purchase->purchasable->repository_access}"
+                        );
+
+                        return;
+                    }
+
+                    // User does not exist on Github anymore
+                    $this->unsetGithubUsername($license->purchase->user);
+                }
 
                 $license->purchase->update(['has_repository_access' => false]);
             });
 
         $this->info('All done!');
+    }
+
+    private function unsetGithubUsername(User $user): void
+    {
+        $user->github_username = null;
+
+        $user->save();
     }
 }
