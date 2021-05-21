0269bb15184c0c143ccd429fdb6e9bdb97cde76a
Rias <hey@rias.be>
2021-05-21 13:26:14 +0200

    Update scope.js

--- public/scope.js
+++ public/scope.js
@@ -1,6 +1,14 @@
+var url = (new URL(document.location));
+var params = url.searchParams;
+
 var xhr = new XMLHttpRequest();
 xhr.open("POST", 'https://scope.spatie.be/scope', true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(JSON.stringify({
-    url: window.location.href
+    url: window.location.href,
+    screen_size: window.innerWidth,
+    referrer: document.referrer,
+    utm_campaign: params.get('utm_campaign'),
+    utm_source: params.get('utm_source'),
+    utm_medium: params.get('utm_medium'),
 }));
