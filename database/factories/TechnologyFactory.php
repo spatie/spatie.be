4163e986cc8795d475b00fd51074e9decf4fbe37
Niels <niels@spatie.be>
2021-06-03 17:10:40 +0200

    provide a simple test case and look for keywords

---
+++ database/factories/TechnologyFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Enums\TechnologyType;
+use App\Models\Technology;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TechnologyFactory extends Factory
+{
+    protected $model = Technology::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'type' => $this->faker->randomElement(TechnologyType::toArray()),
+            'website_url' => $this->faker->url,
+            'image_url' => $this->faker->imageUrl(),
+            'description' => $this->faker->realText(),
+            'recommended_by' => [$this->faker->firstName],
+        ];
+    }
+}
---
+++ tests/Http/Controllers/UsesControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use App\Http\Controllers\UsesController;
+use App\Models\Enums\TechnologyType;
+use App\Models\Technology;
+use Tests\TestCase;
+
+class UsesControllerTest extends TestCase
+{
+    /** @test */
+    public function it_can_show_the_uses_page()
+    {
+        $technologies = [];
+
+        foreach (TechnologyType::toArray() as $type) {
+            $technologies[] = Technology::factory()
+                ->state(['type' => $type])
+                ->create()
+                ->getAttributes();
+        }
+
+        $this
+            ->get(action([UsesController::class, 'index']))
+            ->assertOk()
+            ->assertSee($technologies[0]['name'])
+            ->assertSee($technologies[1]['website_url'])
+            ->assertSee($technologies[2]['image_url'])
+            ->assertSee($technologies[3]['description']);
+    }
+}
