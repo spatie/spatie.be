f5c70e638f2274a9e4d9279e0254e9866fa73c06
freek <freek@spatie.be>
2021-06-09 09:50:05 +0200

    wip

--- resources/views/front/pages/products/partials/purchasedProduct.blade.php
+++ resources/views/front/pages/products/partials/purchasedProduct.blade.php
@@ -9,28 +9,20 @@
         <div>
             @if ($purchasable->getting_started_url)
                 <div>
-                <a class="link-blue link-underline" href="{{ $purchasable->getting_started_url }}">
-                    Getting started
-                </a>
-                </div>
-            @endif
-
-            @if ($purchasable->series->count())
-                <div>
-                <a class="link-blue link-underline" href="{{ route('series.show', $purchasable->series->first()) }}">
-                    Watch course videos
-                </a>
+                    <a class="link-blue link-underline" href="{{ $purchasable->getting_started_url }}">
+                        Getting started
+                    </a>
                 </div>
             @endif
 
             @foreach($purchasable->getMedia('downloads') as $download)
-                    @php
-                        $downloadUrl =  URL::temporarySignedRoute(
-                            'purchase.download',
-                            now()->addMinutes(30),
-                            [$purchasable->product, $purchase, $download]
-                        );
-                    @endphp
+                @php
+                    $downloadUrl =  URL::temporarySignedRoute(
+                        'purchase.download',
+                        now()->addMinutes(30),
+                        [$purchasable->product, $purchase, $download]
+                    );
+                @endphp
 
                 <div>
                     <a class="link-blue link-underline" download="download" href="{{ $downloadUrl }}">
@@ -39,10 +31,20 @@
                 </div>
             @endforeach
 
+            @if ($purchasable->series->count())
+                <div>
+                    <a class="link-blue link-underline"
+                       href="{{ route('series.show', $purchasable->series->first()) }}">
+                        Watch course videos
+                    </a>
+                </div>
+            @endif
+
             @if ($purchasable->repository_access)
                 <div>
-                @if ($purchase->has_repository_access)
-                        <a class="link-blue link-underline" href="https://github.com/{{ $purchasable->repository_access }}">
+                    @if ($purchase->has_repository_access)
+                        <a class="link-blue link-underline"
+                           href="https://github.com/{{ $purchasable->repository_access }}">
                             Visit {{ $purchasable->repository_access }} on GitHub
                         </a>
 
@@ -51,7 +53,7 @@
                             Connect to GitHub to get access to the {{ $purchasable->repository_access }} repo
                         </a>
                     @endif
-                @endif
+                    @endif
                 </div>
         </div>
         <div class="mt-2 text-xs text-gray">
