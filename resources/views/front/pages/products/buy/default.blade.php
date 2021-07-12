f42794f7cc6e73afca25767acc68100c6bd619d4
Willem Van Bockstal <willem@spatie.be>
2021-07-12 09:49:24 +0200

    Discount copy

--- resources/views/front/pages/products/buy/default.blade.php
+++ resources/views/front/pages/products/buy/default.blade.php
@@ -33,7 +33,7 @@
 
         <div class="section md:-mt-12 pt-0 pb-16">
             <div class="flex-0 text-xs text-gray md:mt-6">
-                You'll get 10% extra discount when making a next purchase within 24 hours.
+                Includes a 10% coupon for a follow-up purchase within the next 24 hours.
                 <br/>
                 VAT will be calculated during checkout by
                 <a class="underline" target="_blank" href="https://paddle.com/support/welcome/#vat-tax-handling-and-compliance">Paddle</a>
