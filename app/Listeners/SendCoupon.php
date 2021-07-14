952c3dde3e0ab150c1de707dc3e9f711b087c374
freek <freek@spatie.be>
2021-07-14 12:03:31 +0200

    wip

--- app/Listeners/SendCoupon.php
+++ app/Listeners/SendCoupon.php
@@ -3,7 +3,7 @@
 namespace App\Listeners;
 
 use App\Models\Subscriber;
-use App\Notifications\WelcomeFrontLinePhpWaitingListNotification;
+use App\Notifications\WelcomeTestingLaravelWaitingListNotification;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Spatie\Mailcoach\Domain\Audience\Events\SubscribedEvent;
 use Spatie\Mailcoach\Domain\Audience\Models\Subscriber as MailcoachSubscriber;
@@ -12,8 +12,8 @@ class SendCoupon
 {
     public function handle(SubscribedEvent $event): void
     {
-        if ($event->subscriber->hasTag('front-line-php-waiting-list')) {
-            $this->upcastSubscriber($event->subscriber)->notify(new WelcomeFrontLinePhpWaitingListNotification());
+        if ($event->subscriber->hasTag('testing-laravel-waiting-list')) {
+            $this->upcastSubscriber($event->subscriber)->notify(new WelcomeTestingLaravelWaitingListNotification());
         }
     }
 
