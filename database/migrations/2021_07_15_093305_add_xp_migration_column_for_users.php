73aed7ad8489ef97a6e37d314285795b0c289cc4
Brent Roose <brent@spatie.be>
2021-07-15 09:40:42 +0200

    Add migration command

---
+++ database/migrations/2021_07_15_093305_add_xp_migration_column_for_users.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('xp_migrated')->default(0);
+        });
+    }
+};
