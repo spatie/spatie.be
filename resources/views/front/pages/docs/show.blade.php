5ede87557e2dff2c821bb7b28c90d1f9f58c377b
Willem Van Bockstal <willem@spatie.be>
2021-07-15 17:15:24 +0200

    Add warning for current version (#182)
    * Add warning for current version
    
    * Small shadow
    
    * Copy
    
    * Mention latest version
    
    * Add dynamic versions to version warning
    
    * typos
    
    * Only show for multiple versions
    
    Co-authored-by: Rias <hey@rias.be>

--- resources/views/front/pages/docs/show.blade.php
+++ resources/views/front/pages/docs/show.blade.php
@@ -10,11 +10,31 @@
 
     @include('front.pages.docs.partials.breadcrumbs')
 
+    @php($latestVersion = $repository->aliases->first())
     <section class="wrap md:grid pb-24 gap-12 md:grid-cols-10 items-stretch">
         <div class="z-10 | md:col-span-3 | lg:col-span-2 | print:hidden">
              @include('front.pages.docs.partials.navigation')
         </div>
         <article class="md:col-span-7 lg:col-span-6">
+            @if(count($repository->aliases) > 1)
+            <div class="mb-12 p-4 flex text-sm bg-white bg-opacity-50 rounded-sm md:shadow-light markup-code">
+                <div class="flex-none h-6 w-6 text-orange fill-current">{{ svg('icons/fal-exclamation-circle') }}</div>
+                <div class="ml-4">
+                    <p>
+                    This is the documentation for <strong>{{ $page->alias }}</strong> @if($page->alias !== $latestVersion->slug)but the latest version is
+                        <strong>{{ $latestVersion->slug }}</strong>@endif.
+                    You can switch versions in the menu <span class="hidden md:inline">on the left</span><span class="hidden">/</span><span class="inline md:hidden">at the top</span>.
+                    Check your current version with the following command:
+                    </p>
+                    <div class="mt-2">
+                        <code class="bg-blue-lightest bg-opacity-50 px-2 py-1">
+                            composer show spatie/{{ $repository->slug }}
+                        </code>
+                    </div>
+                </div>
+            </div>
+            @endif
+
             @if($showBigTitle)
                 <div class="mb-16">
                     <h1 class="banner-slogan">
@@ -54,9 +74,9 @@
 
         </article>
         @if(count($tableOfContents))
-            <aside class="hidden lg:block pt-16 w-full pb-16 col-span-2 print-hidden">
+            <aside class="hidden lg:block w-full pb-16 col-span-2 print-hidden">
                 <div class="sticky top-0 py-6">
-                    <div class="pl-4 border-l-2 border-gray-lighter">
+                    <div class="pl-4 border-l-2 border-gray-light border-opacity-50">
                         <h3 class="mb-3 text-gray font-semibold uppercase tracking-wider text-xs">
                             On this page
                         </h3>
