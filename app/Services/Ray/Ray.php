ad5d8f4e1437de7e87c6009f94c189a239e737ab
freek <freek@spatie.be>
2021-07-23 16:29:22 +0200

    wip

--- app/Services/Ray/Ray.php
+++ app/Services/Ray/Ray.php
@@ -36,7 +36,7 @@ class Ray
     public function latestMacosAppleSiliconVersion(): string
     {
         /** Hardcoded for now, replace when building new version */
-        return '1.14.7';
+        return '1.16.0';
 
         return Cache::remember('latest-mac-version', 60, function () {
             $yaml = Http::get("{$this->baseUrl}/arm64/latest-mac.yml")->body();
