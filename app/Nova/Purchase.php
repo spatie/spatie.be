3fb606915975b8f978d8a805f58b0a97d2af84cb
Rias <hey@rias.be>
2021-08-23 15:39:10 +0200

    Add Nova models for assignments

--- app/Nova/Purchase.php
+++ app/Nova/Purchase.php
@@ -9,7 +9,9 @@ use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Purchase extends Resource
 {
@@ -27,16 +29,16 @@ class Purchase extends Resource
     {
         return [
             ID::make()->sortable(),
+            BelongsTo::make('User'),
+            BelongsTo::make('Purchasable'),
+            BelongsTo::make('Bundle'),
 
             Text::make('Paddle Fee')->hideFromIndex(),
             Text::make('Earnings')->hideFromIndex(),
 
-            Boolean::make('Has repository access')->readonly(),
+            HasMany::make('Purchase Assignments', 'assignments', PurchaseAssignment::class),
 
-            BelongsTo::make('Purchasable'),
-            BelongsTo::make('Bundle'),
-            HasMany::make('Licenses')->hideFromIndex()->nullable(),
-            BelongsTo::make('User'),
+            Number::make('Quantity'),
             BelongsTo::make('Receipt')->nullable()->hideFromIndex(),
 
             DateTime::make('Created at'),
