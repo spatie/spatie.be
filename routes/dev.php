aa11b04638ff7ae176c74970c9033749b46b252b
Rias <hey@rias.be>
2021-08-23 11:17:34 +0200

    Add price card to products

--- routes/dev.php
+++ routes/dev.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Domain\Shop\Models\License;
+use App\Domain\Shop\Models\Purchasable;
+use App\Domain\Shop\Notifications\AccountHasBeenCreatedNotification;
 use App\Domain\Shop\Notifications\LicenseExpiredNotification;
 use App\Domain\Shop\Notifications\LicenseExpiredSecondNotification;
 use App\Domain\Shop\Notifications\LicenseIsAboutToExpireNotification;
@@ -23,5 +25,11 @@ Route::prefix('mails')->group(function () {
         $notification = new LicenseExpiredSecondNotification(License::first());
 
         return $notification->toMail(User::first());
+    });
+
+    Route::get('invitation', function () {
+        $notification = new AccountHasBeenCreatedNotification(User::skip(1)->first(), Purchasable::first());
+
+        return $notification->toMail(User::skip(1)->first());
     });
 });
