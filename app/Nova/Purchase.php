25a5ca2f7f87e96c2101ef9b8a60ddb18994459d
Rias <hey@rias.be>
2021-08-23 16:18:35 +0200

    Better purchase overview

--- app/Nova/Purchase.php
+++ app/Nova/Purchase.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
@@ -17,6 +18,8 @@ class Purchase extends Resource
 {
     public static $group = "Products";
 
+    public static $tableStyle = 'tight';
+
     public static $model = EloquentPurchase::class;
 
     //public static $title = 'title';
@@ -25,23 +28,58 @@ class Purchase extends Resource
         'id',
     ];
 
+    public static $with = ['assignments.user', 'bundle', 'purchasable'];
+
     public function fields(Request $request)
     {
         return [
             ID::make()->sortable(),
             BelongsTo::make('User'),
-            BelongsTo::make('Purchasable'),
-            BelongsTo::make('Bundle'),
+            BelongsTo::make('Purchasable')->hideFromIndex(),
+            BelongsTo::make('Bundle')->hideFromIndex(),
+
+            Text::make('Bought', function () use ($request) {
+                $url = '';
+                $resource = null;
+
+                if ($this->purchasable) {
+                    $resource = (new Purchasable($this->purchasable));
+                    $url = '/nova/resources/'.$resource::uriKey().'/'.$resource->getKey();
+                }
+
+                if ($this->bundle) {
+                    $resource = (new Bundle($this->bundle));
+                    $url = '/nova/resources/'.$resource::uriKey().'/'.$resource->getKey();
+                }
+
+                return <<<HTML
+                    <a class='no-underline dim text-primary font-bold' href='{$url}'>
+                        {$resource?->title()}
+                    </a>
+                HTML;
+            })->onlyOnIndex()->asHtml(),
 
             Text::make('Paddle Fee')->hideFromIndex(),
             Text::make('Earnings')->hideFromIndex(),
 
-            HasMany::make('Purchase Assignments', 'assignments', PurchaseAssignment::class),
-
             Number::make('Quantity'),
+            HasMany::make('Purchase Assignments', 'assignments', PurchaseAssignment::class),
+            Text::make('Assignments', function () {
+                return $this->assignments->unique('user_id')->map(function (\App\Domain\Shop\Models\PurchaseAssignment $assignment) {
+                    $resource = (new User($assignment->user));
+                    $url = '/nova/resources/'.$resource::uriKey().'/'.$resource->getKey();
+
+                    return <<<HTML
+                        <a class='no-underline dim text-primary font-bold' href='{$url}'>
+                            {$assignment->user->name}
+                        </a>
+                    HTML;
+                })->join(', ');
+            })->onlyOnIndex()->asHtml(),
+
             BelongsTo::make('Receipt')->nullable()->hideFromIndex(),
 
-            DateTime::make('Created at'),
+            DateTime::make('Created at')->format('d/M/YYYY hh:mm'),
         ];
     }
 }
