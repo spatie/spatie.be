3eb90b2302536ba55c3ddf392151e536d2e50cec
Rias <hey@rias.be>
2021-08-23 15:39:10 +0200

    Add Nova models for assignments

--- app/Nova/License.php
+++ app/Nova/License.php
@@ -6,6 +6,7 @@ use App\Domain\Shop\Models\License as EloquentLicense;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
@@ -19,7 +20,7 @@ class License extends Resource
     public static $title = 'key';
 
     public static $search = [
-        'id', 'key', 'domain',
+        'id', 'key', 'domain', 'name',
     ];
 
     public function fields(Request $request)
@@ -27,14 +28,18 @@ class License extends Resource
         return [
             ID::make()->sortable(),
 
-            BelongsTo::make('Purchasable'),
-            BelongsTo::make('Purchase'),
-            BelongsTo::make('User'),
+            BelongsTo::make('Purchase Assignment', 'assignment', PurchaseAssignment::class),
 
-            Text::make('Key')->hideFromIndex(),
+            Text::make('Key'),
+            Text::make('Domain'),
             Number::make('Satis Authentication Count'),
 
             DateTime::make('expires_at'),
+            DateTime::make('Expiration warning mail sent at'),
+            DateTime::make('Expiration mail sent at'),
+            DateTime::make('Second expiration mail sent at'),
+
+            HasMany::make('Activations'),
         ];
     }
 }
