98063effee7d3e77df7b3457af53ce43a72cf5df
Rias <hey@rias.be>
2021-08-24 10:25:21 +0200

    Add user column to receipt

--- app/Nova/Receipt.php
+++ app/Nova/Receipt.php
@@ -3,7 +3,9 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\MorphTo;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
 use Laravel\Paddle\Receipt as PaddleReceipt;
@@ -24,6 +26,8 @@ class Receipt extends Resource
     {
         return [
             ID::make()->sortable(),
+
+            MorphTo::make('User', 'billable'),
 
             Text::make('Receipt Url'),
             Number::make('Amount'),
