4670b6daf5089407a016d0f507a4ba56ddf4eae5
Willem Van Bockstal <willem@spatie.be>
2021-08-30 14:11:14 +0200

    Add testing banner for docs

--- resources/views/front/pages/docs/banners/testingLaravel.blade.php
+++ resources/views/front/pages/docs/banners/testingLaravel.blade.php
@@ -1,6 +1,6 @@
 <aside class="z-20 fixed flex bottom-0 w-full">
     <div class="mx-auto">
-        <div class="bg-pink text-gray-darkest flex flex-col items-end sm:flex-row sm:items-center justify-center rounded-t p-2 pr-6 shadow-light text-xs sm:text-sm">
+        <div class="bg-pink text-white flex flex-col items-end sm:flex-row sm:items-center justify-center rounded-t p-2 pr-6 shadow-light text-xs sm:text-sm">
             <div class="flex items-center">
                 <div class="mr-2 text-lg icon bg-black bg-opacity-25 text-white rounded-full w-8 flex items-center justify-center h-8">
                     {{ svg('icons/far-graduation-cap') }}
@@ -13,7 +13,7 @@
                 </div>
             </div>
             <a href="https://testing-laravel.com">
-                <button class="mt-2 md:mt-0 ml-4 px-2 py-1 rounded text-purple bg-white uppercase tracking-wide font-semibold">
+                <button class="mt-2 md:mt-0 ml-4 px-2 py-1 rounded text-pink-dark bg-white uppercase tracking-wide font-semibold">
                     Learn&nbsp;more
                 </button>
             </a>
