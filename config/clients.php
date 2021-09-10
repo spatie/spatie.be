e84b6973137481c9dc9d1a748801ee224157e56b
faissaloux <fwahabali@gmail.com>
2021-09-10 17:54:38 +0100

    group clients in config file

---
+++ config/clients.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    "mutsy" => [
+        "website" => "https://mutsy.com",
+        "logo" => "clients/mutsy"
+    ],
+    "krauthammer" => [
+        "website" => "https://krauthammer.com",
+        "logo" => "clients/krauthammer"
+    ],
+    "vlaanderen" => [
+        "website" => "https://vlaanderen.be",
+        "logo" => "clients/vlaanderen"
+    ],
+    "flux50" => [
+        "website" => "https://flux50.com",
+        "logo" => "clients/flux50"
+    ],
+    "skylineeurope" => [
+        "website" => "https://skylineeurope.be",
+        "logo" => "clients/skyline"
+    ],
+    "bodartservicehouse" => [
+        "website" => "https://bodartservicehouse.be",
+        "logo" => "clients/bodart"
+    ],
+    "vluchtelingenwerk" => [
+        "website" => "https://vluchtelingenwerk.be",
+        "logo" => "clients/vluchtelingenwerk"
+    ],
+    "vrijwilligerswerk" => [
+        "website" => "https://vrijwilligerswerk.be",
+        "logo" => "clients/vrijwilligerswerk"
+    ]
+];
--- resources/views/front/pages/home/partials/clients.blade.php
+++ resources/views/front/pages/home/partials/clients.blade.php
@@ -5,13 +5,10 @@
         </h2>
     </div>
     <div class="wrap grid grid-cols-2 mt-8 col-gap-8 row-gap-2 | md:grid-cols-4 md:col-gap-16">
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://mutsy.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/mutsy') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://krauthammer.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/krauthammer') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://vlaanderen.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vlaanderen') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://flux50.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/flux50') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://skylineeurope.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/skyline') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://bodartservicehouse.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/bodart') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://vluchtelingenwerk.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vluchtelingenwerk') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://vrijwilligerswerk.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vrijwilligerswerk') }}</a></div>
+        @foreach(config('clients') as $client)
+            <div class="text-center">
+                <a class="w-full max-w-logoclient inline-block" href="{{ $client['website'] }}" target="_blank" rel="noreferrer noopener">{{ svg($client['logo']) }}</a>
+            </div>
+        @endforeach
     </div>
 </section>
