5476216e72532531af930c0e7bb5e143fab83475
Rias <hey@rias.be>
2021-10-19 16:02:29 +0200

    wip

--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -23,6 +23,16 @@ class DocsController
         abort_if(is_null($repository), 404, 'Repository not found');
 
         if ($alias) {
+            preg_match('/v\d+/', $alias, $matches);
+
+            if (! count($matches)) {
+                $latest = $repository->aliases->keys()->first();
+                $slug = $alias;
+                $alias = $latest;
+
+                return redirect()->action([DocsController::class, 'show'], [$repository->slug, $alias, $slug]);
+            }
+
             $alias = $repository->getAlias($alias);
 
             abort_if(is_null($alias), 404, 'Alias not found');
