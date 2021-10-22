e10e4d81a611af774836860655d6acc4caa4a1ac
freek <freek@spatie.be>
2021-10-23 01:46:18 +0200

    wip

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,7 +15,7 @@ class EventServiceProvider extends ServiceProvider
             SendEmailVerificationNotification::class,
         ],
         SubscribedEvent::class => [
-            SendCoupon::class,
+            //SendCoupon::class,
         ],
     ];
 }
