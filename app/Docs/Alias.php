28af3fa6b611819ad20550e19ee2f87aa2b4b97a
faissaloux <fwahabali@gmail.com>
2021-11-25 22:56:55 +0100

    refactor alias class

--- app/Docs/Alias.php
+++ app/Docs/Alias.php
@@ -6,27 +6,11 @@ use Illuminate\Support\Collection;
 
 class Alias
 {
-    public string $slug;
-
-    public string $slogan;
-
-    public string $branch;
-
-    public string $githubUrl;
-
-    public Collection $pages;
-
     public function __construct(
-        string $slug,
-        string $slogan,
-        string $branch,
-        string $githubUrl,
-        Collection $pages
-    ) {
-        $this->slug = $slug;
-        $this->slogan = $slogan;
-        $this->branch = $branch;
-        $this->githubUrl = $githubUrl;
-        $this->pages = $pages;
-    }
+        public string $slug,
+        public string $slogan,
+        public string $branch,
+        public string $githubUrl,
+        public Collection $pages
+    ) {}
 }
