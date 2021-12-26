3a9a9007d763d857c0adcf8388d84332e7944990
freek <freek@spatie.be>
2021-12-26 21:11:15 +0100

    wip

--- app/Nova/Actions/RegenerateLicenseKeyAction.php
+++ app/Nova/Actions/RegenerateLicenseKeyAction.php
@@ -5,9 +5,10 @@ namespace App\Nova\Actions;
 use App\Nova\License;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use Laravel\Nova\Actions\DestructiveAction;
 use Laravel\Nova\Fields\ActionFields;
 
-class RegenerateLicenseKeyAction
+class RegenerateLicenseKeyAction extends DestructiveAction
 {
     public function handle(ActionFields $fields, Collection $models)
     {
