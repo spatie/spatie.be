ba82ecc3fecf36ff35ede955f2cb423040e50b43
Willem Van Bockstal <willem@spatie.be>
2022-02-17 13:03:15 +0100

    React copy

--- resources/views/front/pages/vacancies/partials/list.blade.php
+++ resources/views/front/pages/vacancies/partials/list.blade.php
@@ -3,14 +3,13 @@
     Why don't you squeeze yourself in?
 </p>
 
-{{--
+
 <div class="mt-4">
-    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'react-developer') }}">React developer</a>
+    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'react-engineer') }}">React Engineer</a>
     <div class="text-xs text-gray">React with a hint of Vanilla JS</div>
     <div class="text-xs text-gray">Antwerp / Partially remote</div>
-</div>--}}
-<p class="mt-4">
-    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'spontaneous-application') }}">Spontaneous application</a>
-    <br>
-    <span class="text-xs text-gray">Antwerp / Partially remote</span>
-</p>
+</div>
+<div class="mt-4">
+    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'spontaneous-application') }}">Spontaneous Application</a>
+    <div class="text-xs text-gray">Antwerp / Partially remote</div>
+</div>
