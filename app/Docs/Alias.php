922edeae33c09eef4c065784e7eb1d197083acfe
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2022-02-18 11:35:15 +0100

    add version numer to Alias VO

--- app/Docs/Alias.php
+++ app/Docs/Alias.php
@@ -6,12 +6,24 @@ use Illuminate\Support\Collection;
 
 class Alias
 {
-    public function __construct(
+    private function __construct(
         public string $slug,
         public string $slogan,
         public string $branch,
+        public int $versionNumber,
         public string $githubUrl,
         public Collection $pages
-    ) {
+    ) {}
+
+    public static function fromDocumentationPage(DocumentationPage $page, Collection $pages): self
+    {
+        return new self(
+            slug: $page->title,
+            slogan: $page->slogan,
+            branch: $page->branch,
+            versionNumber: (int) filter_var($page->title, FILTER_SANITIZE_NUMBER_INT),
+            githubUrl: $page->githubUrl,
+            pages: $pages,
+        );
     }
 }
