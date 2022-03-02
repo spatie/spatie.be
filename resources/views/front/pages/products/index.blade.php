75c3a53d17a8df4b983ba1837358aba7f72d0043
Rias <hey@rias.be>
2022-03-02 09:11:49 +0100

    add label for non visible products

--- resources/views/front/pages/products/index.blade.php
+++ resources/views/front/pages/products/index.blade.php
@@ -10,7 +10,7 @@
                  Welcome in <br>our store
              </h1>
 
-            <!-- 
+            <!--
             <p class="banner-intro">
                 Applications and digital courses built for modern developers
             </p>
@@ -70,8 +70,8 @@
                                 <div class="-mt-8 pb-6 transition-transform transform ease-in-out group-hover:-translate-y-2 duration-200">
                                     <div class="shadow-md group-hover:shadow-lg">{{ $product->getFirstMedia('product-image') }}</div>
                                 </div>
-                           
-                                <h2 class="title-sm link-black link-underline-hover">{{ $product->title }}</h2>
+
+                                <h2 class="title-sm link-black link-underline-hover">{{ $product->title }} @if(! $product->visible && current_user()?->hasAccessToUnReleasedProducts())(only visible for Spatie users)@endif</h2>
                             </a>
 
                             <p class="my-4 flex items-center space-x-4">
@@ -92,13 +92,13 @@
                                 </p>
                             @endif
 
-                            
+
 
                             <p class="mt-4">{{ $product->formattedDescription }}</p>
-                            
 
 
-                            
+
+
                         </div>
                     @endforeach
                 </div>
