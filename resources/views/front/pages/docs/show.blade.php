e0730636a05b332ffed96ff7bf788a12c7299a24
Freek Van der Herten <freek@spatie.be>
2022-03-09 16:29:32 +0100

    wip

--- resources/views/front/pages/docs/show.blade.php
+++ resources/views/front/pages/docs/show.blade.php
@@ -38,6 +38,9 @@
                 </div>
             </div>
             @endif
+            <div class="mx-8">
+                <img alt="Support Ukraine" src="https://github-ads.s3.eu-central-1.amazonaws.com/support-ukraine.svg" />
+            </div>
 
             @if($showBigTitle)
                 <div class="mb-16">
@@ -70,8 +73,6 @@
                     </ol>
                 </div>
             @endif
-
-            <img alt="Support Ukraine" src="https://github-ads.s3.eu-central-1.amazonaws.com/support-ukraine.svg" />
 
             <div class="markup markup-titles markup-lists markup-tables markup-code markup-embeds
              links-blue links-underline">
