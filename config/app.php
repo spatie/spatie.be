a303fdbc21b28af1f67b3443e51c0ff81d388726
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2022-03-11 14:37:21 +0100

    remove unused config

--- config/app.php
+++ config/app.php
@@ -213,7 +213,7 @@ return [
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
         'DB' => Illuminate\Support\Facades\DB::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Debugbar' => Barryvdh\Debugbar\LaravelDebugbar::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
         'File' => Illuminate\Support\Facades\File::class,
