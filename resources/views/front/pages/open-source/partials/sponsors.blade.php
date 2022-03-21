37803fd3a67c83b042ec9c5c5199bd3855c9339e
Willem Van Bockstal <willem@spatie.be>
2022-03-21 16:14:15 +0100

    Add GH sponsors

--- resources/views/front/pages/open-source/partials/sponsors.blade.php
+++ resources/views/front/pages/open-source/partials/sponsors.blade.php
@@ -2,7 +2,7 @@
     <div class="filter-backdrop bg-opaque px-16 xl:px-32 pt-8 pb-12 max-w-md mx-auto shadow-lg rounded-sm">
         <h4 class="text-center text-gray-dark text-xs leading-snug">
             Our open-source efforts are supported 
-            <br>by the following <a class="font-bold" href="https://github.com/sponsors/spatie">GitHub sponsors</a>
+            <br>by the following <a class="font-bold" href="https://github.com/sponsors/spatie">premium GitHub sponsors</a>
         </h4>
 
         <div class="mt-4 flex items-center justify-between gap-4">
