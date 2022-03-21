46fcd895ecf3ccc0715b11952c097cec12d7a4c8
Willem Van Bockstal <willem@spatie.be>
2022-03-21 16:35:20 +0100

    Add GH sponsor link

--- resources/views/front/pages/open-source/partials/sponsors.blade.php
+++ resources/views/front/pages/open-source/partials/sponsors.blade.php
@@ -1,11 +1,11 @@
 <div class="wrap mb-16">
-    <div class="filter-backdrop bg-opaque px-16 xl:px-32 py-12 md:pt-8 max-w-md mx-auto shadow-lg rounded-sm">
-        <h4 class="text-center text-gray-dark text-xs leading-snug">
+    <div class="filter-backdrop bg-opaque px-16 xl:px-32 py-8 max-w-md mx-auto shadow-lg rounded-sm">
+        <h4 class="text-center text-black text-sm leading-snug">
             Our open-source efforts are supported 
             <br>by the following <a class="font-bold" href="https://github.com/sponsors/spatie">premium GitHub sponsors</a>
         </h4>
 
-        <div class="mt-6 md:mt-4 flex flex-col md:flex-row items-center justify-between gap-4">
+        <div class="mt-6 flex flex-col md:flex-row items-center justify-between gap-4">
             <a class="transition duration-300 transform hover:scale-105" href="https://laravel.com">
                 <div class="w-32 h-8">
                     @svg('sponsors/laravel')
@@ -20,5 +20,9 @@
                 <img class="w-32 h-8 object-contain" src="images/sponsors/scout.png" />
             </a>
         </div>
+
     </div>
+    <h4 class="mt-4 text-center text-gray text-xs leading-snug">
+        Enjoying our packages? <a class="underline" href="https://github.com/sponsors/spatie">Become a sponsor</a>
+    </h4>
 </div>
