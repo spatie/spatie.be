16bd68e97d01e2a011ebf3083e932aa9014c41e2
Willem Van Bockstal <willem@spatie.be>
2022-03-21 16:25:53 +0100

    Add GH sponsors reponsive

--- resources/views/front/pages/open-source/partials/sponsors.blade.php
+++ resources/views/front/pages/open-source/partials/sponsors.blade.php
@@ -1,11 +1,11 @@
 <div class="wrap mb-16">
-    <div class="filter-backdrop bg-opaque px-16 xl:px-32 pt-8 pb-12 max-w-md mx-auto shadow-lg rounded-sm">
+    <div class="filter-backdrop bg-opaque px-16 xl:px-32 py-12 md:pt-8 max-w-md mx-auto shadow-lg rounded-sm">
         <h4 class="text-center text-gray-dark text-xs leading-snug">
             Our open-source efforts are supported 
             <br>by the following <a class="font-bold" href="https://github.com/sponsors/spatie">premium GitHub sponsors</a>
         </h4>
 
-        <div class="mt-4 flex flex-col md:flex-row items-center justify-between gap-4">
+        <div class="mt-6 md:mt-4 flex flex-col md:flex-row items-center justify-between gap-4">
             <a class="transition duration-300 transform hover:scale-105" href="https://laravel.com">
                 <div class="w-32 h-8">
                     @svg('sponsors/laravel')
@@ -16,7 +16,7 @@
                     @svg('sponsors/loadforge')
                 </div>
             </a>
-            <a class="transition duration-300 transform hover:scale-105" href="https://laravel.com">
+            <a class="transition duration-300 transform hover:scale-105" href="https://scoutapm.com/">
                 <img class="w-32 h-8 object-contain" src="images/sponsors/scout.png" />
             </a>
         </div>
