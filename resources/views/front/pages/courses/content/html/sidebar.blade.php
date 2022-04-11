631b484f7172080a6c093312bf42a8f4c7cc2ca2
vicdenys <vicdenys@hotmail.com>
2022-04-11 16:56:06 +0200

    wip

--- resources/views/front/pages/courses/content/html/sidebar.blade.php
+++ resources/views/front/pages/courses/content/html/sidebar.blade.php
@@ -43,9 +43,7 @@
     <ul class="text-xs grid bg-white p-2 rounded-sm shadow links-blue markup-list-compact">
         <li class="bg-white py-2 rounded-sm {{ request()->routeIs('series.show') ? " bg-paper " : "" }}">
             <a class="flex items-center gap-4" href="{{ route('series.show', [$series]) }}">
-                    @if (request()->routeIs('series.show'))
-                        <div class="w-1 h-1 absolute bg-gray-light rounded-full "></div>
-                    @endif
+                    
                 <span class="mr-1 text-black {{ request()->routeIs('series.show') ? "
                     font-sans-bold " : "" }}">Introduction</span>
             </a>
@@ -81,9 +79,7 @@
                         @foreach($lessonsPerChapter as $lessonInChapter)
                             <li class="bg-white  py-2 rounded-sm flex items-center gap-2 {{ isset($lesson) && $lesson->id === $lessonInChapter->id ? "
                     font-sans-bold bg-paper text-white" : "" }}">
-                                    @if (isset($lesson) && $lesson->id === $lessonInChapter->id ))
-                                    <div class="w-1 h-1 absolute bg-gray-light rounded-full "></div>
-                                    @endif
+                                    
                                     
                                 <a class="flex items-center gap-2" href="{{ route('courses.show', [$series, $lessonInChapter]) }}">
                                     <span class="mr-1">{{ $lessonInChapter->title }}</span>
