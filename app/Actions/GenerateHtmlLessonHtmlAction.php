bfac6f0b3943a992c43ccf34b987efaede709dcf
Freek Van der Herten <freek@spatie.be>
2022-04-12 14:06:53 +0200

    wip

--- app/Actions/GenerateHtmlLessonHtmlAction.php
+++ app/Actions/GenerateHtmlLessonHtmlAction.php
@@ -3,13 +3,16 @@
 namespace App\Actions;
 
 use App\Models\HtmlLesson;
+use League\CommonMark\Extension\Table\TableExtension;
 use Spatie\LaravelMarkdown\MarkdownRenderer;
 
 class GenerateHtmlLessonHtmlAction
 {
     public function execute(HtmlLesson $htmlLesson)
     {
-        $html = app(MarkdownRenderer::class)->toHtml($htmlLesson->markdown);
+        $html = app(MarkdownRenderer::class)
+            ->addExtension(new TableExtension())
+            ->toHtml($htmlLesson->markdown);
 
         $htmlLesson->updateQuietly([
             'html' => $html,
