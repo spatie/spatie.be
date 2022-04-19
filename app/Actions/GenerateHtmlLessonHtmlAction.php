4715173c62ad0a3cb84ecaeff46ef0e84c8dccc6
Freek Van der Herten <freek@spatie.be>
2022-04-19 17:49:00 +0200

    wip

--- app/Actions/GenerateHtmlLessonHtmlAction.php
+++ app/Actions/GenerateHtmlLessonHtmlAction.php
@@ -3,6 +3,7 @@
 namespace App\Actions;
 
 use App\Models\HtmlLesson;
+use Illuminate\Support\Str;
 use League\CommonMark\Extension\Table\TableExtension;
 use Spatie\LaravelMarkdown\MarkdownRenderer;
 
@@ -13,6 +14,12 @@ class GenerateHtmlLessonHtmlAction
         $html = app(MarkdownRenderer::class)
             ->addExtension(new TableExtension())
             ->toHtml($htmlLesson->markdown);
+
+        $html = Str::of($html)
+            ->replace('[good]', '<div class="shiki-good">')
+            ->replace('[bad]', '<div class="shiki-bad">')
+            ->replace(['[/good]', '[/bad]'], '</div>')
+            ->toString();
 
         $htmlLesson->updateQuietly([
             'html' => $html,
