e33866bde5a43710a0d8184f6bb8079e97d53628
Alex Vanderbist <alex.vanderbist@gmail.com>
2022-04-25 14:31:24 +0200

    Report connection exception too

--- app/Support/FreeGeoIp/FreeGeoIp.php
+++ app/Support/FreeGeoIp/FreeGeoIp.php
@@ -26,7 +26,9 @@ class FreeGeoIp
 
                 return $countryCode;
             });
-        } catch (ConnectionException) {
+        } catch (ConnectionException $exception) {
+            report($exception);
+
             return 'US';
         }
     }
