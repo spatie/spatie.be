cb305a51bbe5252032192670a508f58b94d6a513
Willem Van Bockstal <willem@spatie.be>
2022-05-02 16:13:18 +0200

    Shiki diff lines

--- resources/views/front/pages/docs/show.blade.php
+++ resources/views/front/pages/docs/show.blade.php
@@ -21,7 +21,7 @@
         </div>
         <article class="md:col-span-7 lg:col-span-6">
             @if(count($repository->aliases) > 1)
-            <div class="mb-12 p-4 flex text-sm bg-white bg-opacity-50 rounded-sm md:shadow-light markup-code">
+            <div class="mb-12 p-4 flex text-sm bg-white bg-opacity-50 rounded-sm md:shadow-light markup-shiki">
                 <div class="flex-none h-6 w-6 text-orange fill-current">{{ svg('icons/fal-exclamation-circle') }}</div>
                 <div class="ml-4">
                     <p>
@@ -78,7 +78,7 @@
                 </div>
             @endif
 
-            <div class="markup markup-titles markup-lists markup-tables markup-code markup-embeds
+            <div class="markup markup-titles markup-lists markup-tables markup-shiki markup-embeds
              links-blue links-underline">
                 {!! $page->contents !!}
             </div>
