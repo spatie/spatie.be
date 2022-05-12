2490745d25bfb86abfd564459c5feabd8179ef47
Willem Van Bockstal <willem@spatie.be>
2022-05-12 10:18:54 +0200

    Vacancy update

--- resources/views/front/pages/vacancies/partials/list.blade.php
+++ resources/views/front/pages/vacancies/partials/list.blade.php
@@ -6,24 +6,24 @@
 <div class="mt-4">
     <a class="link-underline link-blue" href="{{ route('vacancies.show', 'frontend-designer') }}">Frontend Designer</a>
     <div class="text-xs text-gray">Mixing style &amp; technology</div>
-    <div class="text-xs text-gray">Antwerp / Partially remote</div>
+    <div class="text-xs text-gray">Full-time / Partially remote</div>
 </div>
 <div class="mt-4">
     <a class="link-underline link-blue" href="{{ route('vacancies.show', 'react-engineer') }}">React Engineer</a>
     <div class="text-xs text-gray">React with a hint of Vanilla JS</div>
-    <div class="text-xs text-gray">Antwerp / Partially remote</div>
+    <div class="text-xs text-gray">Full-time / Partially remote</div>
 </div>
 {{-- <div class="mt-4">
     <a class="link-underline link-blue" href="{{ route('vacancies.show', 'digital-marketeer') }}">Digital Marketeer</a>
     <div class="text-xs text-gray">Promoting our portfolio</div>
-    <div class="text-xs text-gray">Antwerp / Fully or partially remote</div>
+    <div class="text-xs text-gray">Part-time / Fully or partially remote</div>
 </div> --}}
 <div class="mt-4">
     <a class="link-underline link-blue" href="{{ route('vacancies.show', 'backend-engineer') }}">Backend Engineer</a>
     <div class="text-xs text-gray">PHP and Laravel</div>
-    <div class="text-xs text-gray">Antwerp / Partially remote</div>
+    <div class="text-xs text-gray">Full-time / Partially remote</div>
 </div>
 <div class="mt-4">
     <a class="link-underline link-blue" href="{{ route('vacancies.show', 'spontaneous-application') }}">Spontaneous Application</a>
-    <div class="text-xs text-gray">Antwerp / Partially remote</div>
+    <div class="text-xs text-gray">Full-time / Partially remote</div>
 </div>
