32982319808a4ae1b65ff81298f58b13fb5f46bd
Freek Van der Herten <freek@spatie.be>
2022-06-28 20:01:01 +0200

    wip

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import laravel from 'laravel-vite-plugin'
+import {defineConfig} from 'vite'
+import {homedir} from 'os'
+import {resolve} from 'path'
+
+let host = 'spatie.be.test'
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            'resources/js/front/app.js',
+        ]),
+    ],
+    server: detectServerConfig(host),
+})
+
+function detectServerConfig(host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certPath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+    if (!fs.existsSync(keyPath)) {
+        return {}
+    }
+
+    if (!fs.existsSync(certPath)) {
+        return {}
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certPath),
+        },
+    }
+}
--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/front/app.js', 'public/js')
-    .postCss('resources/css/front/front.css', 'public/css', [require('tailwindcss')])
-    .version();
