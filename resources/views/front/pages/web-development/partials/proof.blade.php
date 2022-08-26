444d248946fa8a72facde02e9e6fa519879923a0
Freek Van der Herten <freek@spatie.be>
2022-08-26 09:25:45 +0200

    wip

--- resources/views/front/pages/web-development/partials/proof.blade.php
+++ resources/views/front/pages/web-development/partials/proof.blade.php
@@ -7,7 +7,6 @@
                 </h2>
                 <ul class="text-2xl links-black links-underline bullets bullets-blue">
                         <li><span class="icon">{{ svg('icons/far-angle-right') }}</span> <a href="{{ route('open-source.packages') }}">Our open source work</a></li>
-                        <li><span class="icon">{{ svg('icons/far-angle-right') }}</span> <a href="http://git-awards.com/users?utf8=✓&type=world&language=php" target="_blank" rel="nofollow noreferrer noopener">Top ranking on GitHub</a></li>
                         <li><span class="icon">{{ svg('icons/far-angle-right') }}</span> <a href="{{ route('about') }}#team">A skilled team</a></li>
                     </ul>
             </div>
