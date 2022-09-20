41e8f532bc976f155a035339e1fc7c4aa11cd08c
Rias <hey@rias.be>
2022-09-20 12:55:33 +0200

    Add in a permanent redirect for the mailcoach routes

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,8 @@ use App\Http\Controllers\CoursesController;
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/mailcoach/{any}', 'https://spatie.mailcoach.app/{any}')->where('any', '.*');
+
 Route::get('stop-impersonation', StopImpersonationController::class)->name('stop-impersonation');
 
 Route::feeds();
