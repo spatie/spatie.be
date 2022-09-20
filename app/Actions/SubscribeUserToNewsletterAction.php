16f10328636b7e37516b36158840645d29bbc2b5
Rias <hey@rias.be>
2022-09-20 11:41:14 +0200

    Update subscribe action

--- app/Actions/SubscribeUserToNewsletterAction.php
+++ app/Actions/SubscribeUserToNewsletterAction.php
@@ -3,18 +3,17 @@
 namespace App\Actions;
 
 use App\Models\User;
-use Spatie\Mailcoach\Domain\Audience\Models\EmailList;
-use Spatie\Mailcoach\Domain\Audience\Models\Subscriber;
+use Illuminate\Support\Facades\Http;
 
 class SubscribeUserToNewsletterAction
 {
     public function execute(User $user): User
     {
-        $emailList = EmailList::firstWhere('name', 'Spatie');
-
-        Subscriber::createWithEmail($user->email)
-            ->skipConfirmation()
-            ->subscribeTo($emailList);
+        Http::withToken(config('services.mailcoach.token'))
+            ->post('https://spatie.mailcoach.app/api/email-lists/4af46b59-3784-41a5-9272-6da31afa3a02/subscribers', [
+                'email' => $user->email,
+                'skip_confirmation' => true,
+            ]);
 
         return $user;
     }
