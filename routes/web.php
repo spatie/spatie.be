b0f06d5517ea03d94f79a0cc83565fa4e8c47359
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2022-10-03 16:49:08 +0200

    remove routes

--- routes/web.php
+++ routes/web.php
@@ -68,10 +68,6 @@ Route::view('web-development', 'front.pages.web-development.index')->name('web-d
 
 Route::prefix('about-us')->group(function () {
     Route::view('/', 'front.pages.about.index')->name('about');
-
-    collect(Member::get())->pluck('first_name')->each(function (string $personName) {
-        Route::permanentRedirect($personName, "/about-us/#{$personName}");
-    });
 });
 
 Route::prefix('products')->group(function () {
