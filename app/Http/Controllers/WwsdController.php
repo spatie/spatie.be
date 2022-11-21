11659905434e7194a9566ac15d55a4628cf4b554
Freek Van der Herten <freek@spatie.be>
2022-11-21 08:08:21 +0100

    wip

--- app/Http/Controllers/WwsdController.php
+++ app/Http/Controllers/WwsdController.php
@@ -35,10 +35,17 @@ class WwsdController
     {
         return collect([
             [
+                'youtube_id' => 'JdrSqdBJZQI',
+                'slug' => 'day-1',
+                'title' => 'Welcome to World Wide Spatie Discounts',
+                'main' => true,
+                'thumbnail' => 'https://i.ytimg.com/vi/JdrSqdBJZQI/hqdefault.jpg',
+            ],
+            [
                 'youtube_id' => 'R8z1b3f4j5I',
                 'slug' => 'intro',
                 'title' => 'Introducing World Wide Spatie Discounts',
-                'main' => true,
+                'main' => false,
                 'thumbnail' => 'https://i.ytimg.com/vi/R8z1b3f4j5I/hqdefault.jpg',
             ],
         ]);
