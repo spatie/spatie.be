fe54a4ffe9ec5f72a8c74a7b32f1b50c25abf7f9
Freek Van der Herten <freek@spatie.be>
2022-12-09 14:44:42 +0100

    wip

--- resources/views/api/helpSpace.blade.php
+++ resources/views/api/helpSpace.blade.php
@@ -1,5 +1,9 @@
 <div class="mt-4">
     <h2 class="flex-grow spm-ticket-sidebar-head mb-px">Spatie</h2>
+    <div>
+        <a class="text-sm" href="https://spatie.be/nova/resources/users/{{ $user->id }}">View in Nova</a>
+    </div>
+
     <div class="relative flex-wrap w-full mt-2 bg-gray-200 rounded-lg px-4 py-4 3xl:mr-2">
         <h3 class="flex-grow spm-ticket-sidebar-head mb-px">Purchases</h3>
         <div class="text-sm mb-4">
@@ -12,6 +16,5 @@
             </ul>
 
         </div>
-        <a class="text-sm" href="https://spatie.be/nova/resources/users/{{ $user->id }}">View in Nova</a>
     </div>
 </div>
