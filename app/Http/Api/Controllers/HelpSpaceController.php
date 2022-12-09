eb12429221ed53cf12f2fdaa9561c3d89a047304
Rias <hey@rias.be>
2022-12-09 12:15:17 +0100

    add purchases

--- app/Http/Api/Controllers/HelpSpaceController.php
+++ app/Http/Api/Controllers/HelpSpaceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Api\Controllers;
 
+use App\Domain\Shop\Models\Purchasable;
+use App\Domain\Shop\Models\Purchase;
 use App\Http\Requests\HelpSpaceRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Http;
@@ -21,8 +23,27 @@ class HelpSpaceController
             return 'No user found at spatie.be';
         }
 
-        // $userInfo = view('add-view');
+        $purchases = $user->purchases
+            ->map(fn (Purchase $purchase) => $purchase->getPurchasables())
+            ->map(fn (Purchasable $purchasable) => $purchasable->title)
+            ->unique()
+            ->implode(', ');
 
-        return response()->json(['html' => $mailcoachHtml]);
+
+
+        // $userInfo = view('add-view');
+        $spatieHtml = <<<html
+            <div>
+                <h2 class="flex-grow spm-ticket-sidebar-head mb-px">Spatie</h2>
+                <div class="relative flex-wrap w-full mt-2 bg-gray-200 rounded-lg px-4 py-4 3xl:mr-2">
+                    <h3 class="flex-grow spm-ticket-sidebar-head mb-px">Purchases</h3>
+                    <div class="text-sm mb-4">{$purchases}</div>
+                </div>
+            </div>
+        html;
+
+        $spatieHtml .= $mailcoachHtml;
+
+        return response()->json(['html' => $spatieHtml]);
     }
 }
