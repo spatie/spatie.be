b8ccb801dff5fedf4a982997718d10ab8385dff2
Freek Van der Herten <freek@spatie.be>
2022-12-09 14:41:11 +0100

    wip

--- resources/views/api/helpSpace.blade.php
+++ resources/views/api/helpSpace.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="mt-4">
     <h2 class="flex-grow spm-ticket-sidebar-head mb-px">Spatie</h2>
     <div class="relative flex-wrap w-full mt-2 bg-gray-200 rounded-lg px-4 py-4 3xl:mr-2">
         <h3 class="flex-grow spm-ticket-sidebar-head mb-px">Purchases</h3>
