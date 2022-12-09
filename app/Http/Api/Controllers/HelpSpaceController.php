24e2717b6fe9fef420833e568df36fa33c768681
Freek Van der Herten <freek@spatie.be>
2022-12-09 14:12:48 +0100

    wip

--- app/Http/Api/Controllers/HelpSpaceController.php
+++ app/Http/Api/Controllers/HelpSpaceController.php
@@ -6,17 +6,20 @@ use App\Domain\Shop\Models\Purchasable;
 use App\Domain\Shop\Models\Purchase;
 use App\Http\Requests\HelpSpaceRequest;
 use App\Models\User;
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class HelpSpaceController
 {
     public function __invoke(HelpSpaceRequest $request)
     {
-        $mailcoachHtml = Http::withHeaders(['signature' => $request->header('signature')])
-            ->withBody($request->getContent(), $request->getContentType())
-            ->post('https://mailcoach.app/api/help-space')
-            ->json('html', '');
+        $html = $this->getSpatieContent($request) . $this->getMailCoachContent($request);
 
+        return response()->json(['html' => $html]);
+    }
+
+    public function getSpatieContent(HelpSpaceRequest $request): string
+    {
         $user = User::firstWhere('email', $request->email());
 
         if (! $user) {
@@ -29,21 +32,20 @@ class HelpSpaceController
             ->unique()
             ->implode(', ');
 
+        return view('api.helpSpace', compact('user', 'purchases'))->render();
+    }
 
+    protected function getMailCoachContent(HelpSpaceRequest $request): string
+    {
+        try {
+            return Http::withHeaders(['signature' => $request->header('signature')])
+                ->withBody($request->getContent(), $request->getContentType())
+                ->post('https://mailcoach.app/api/help-space')
+                ->json('html', '');
+        }
+        catch (Exception) {
+            return '';
+        }
 
-        // $userInfo = view('add-view');
-        $spatieHtml = <<<html
-            <div>
-                <h2 class="flex-grow spm-ticket-sidebar-head mb-px">Spatie</h2>
-                <div class="relative flex-wrap w-full mt-2 bg-gray-200 rounded-lg px-4 py-4 3xl:mr-2">
-                    <h3 class="flex-grow spm-ticket-sidebar-head mb-px">Purchases</h3>
-                    <div class="text-sm mb-4">{$purchases}</div>
-                </div>
-            </div>
-        html;
-
-        $spatieHtml .= $mailcoachHtml;
-
-        return response()->json(['html' => $spatieHtml]);
     }
 }
---
+++ resources/views/api/helpSpace.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h2 class="flex-grow spm-ticket-sidebar-head mb-px">Spatie</h2>
+    <div class="relative flex-wrap w-full mt-2 bg-gray-200 rounded-lg px-4 py-4 3xl:mr-2">
+        <h3 class="flex-grow spm-ticket-sidebar-head mb-px">Purchases</h3>
+        <div class="text-sm mb-4">{$purchases}</div>
+    </div>
+</div>
