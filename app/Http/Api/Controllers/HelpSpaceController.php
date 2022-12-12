67c2b48b1cd4dd03d20b9e82c0304cbc9f96f4f9
Freek Van der Herten <freek@spatie.be>
2022-12-12 12:05:51 +0100

    add flare info

--- app/Http/Api/Controllers/HelpSpaceController.php
+++ app/Http/Api/Controllers/HelpSpaceController.php
@@ -5,13 +5,18 @@ namespace App\Http\Api\Controllers;
 use App\Http\Requests\HelpSpaceRequest;
 use App\Models\User;
 use Exception;
+use Illuminate\Http\Client\Pool;
 use Illuminate\Support\Facades\Http;
 
 class HelpSpaceController
 {
     public function __invoke(HelpSpaceRequest $request)
     {
-        $html = $this->getMailCoachContent($request) . $this->getSpatieContent($request);
+        [$mailcoachContent, $flareContent] = $this->getExternalContent($request);
+
+        $html = $mailcoachContent
+            . $flareContent
+            . $this->getSpatieContent($request);
 
         return response()->json(['html' => $html]);
     }
@@ -27,15 +32,22 @@ class HelpSpaceController
         return view('api.helpSpace', compact('user'))->render();
     }
 
-    protected function getMailCoachContent(HelpSpaceRequest $request): string
+    protected function getExternalContent(HelpSpaceRequest $request): array
     {
-        try {
-            return Http::withHeaders(['signature' => $request->header('signature')])
+        $responses = Http::pool(fn (Pool $pool) => [
+            $pool->withHeaders(['signature' => $request->header('signature')])
                 ->withBody($request->getContent(), $request->getContentType())
                 ->post('https://mailcoach.app/api/help-space')
-                ->json('html', '');
-        } catch (Exception) {
-            return '';
-        }
+                ->json('html', ''),
+            $pool->withHeaders(['signature' => $request->header('signature')])
+                ->withBody($request->getContent(), $request->getContentType())
+                ->post('https://flareapp.io/api/help-space')
+                ->json('html', ''),
+            ]);
+
+        return [
+            'mailcoachContent' => $responses[0]->ok() ? $responses[0] : '',
+            'flareContent' => $responses[1]->ok() ? $responses[1] : '',
+        ];
     }
 }
