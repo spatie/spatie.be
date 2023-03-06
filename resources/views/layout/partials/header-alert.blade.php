44e62b82bf7ee39992a1d5015ebb05e025fbdf3b
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-03-06 14:01:00 +0100

    change copy on banner

--- resources/views/layout/partials/header-alert.blade.php
+++ resources/views/layout/partials/header-alert.blade.php
@@ -1,5 +1,5 @@
 <div class="gradient gradient-pink text-white items-center justify-center p-2 text-center text-sm">
     <div class="mt-px ml-1">
-        Launching a new design for <a href="https://flareapp.io" target="_blank" class="underline">Flare</a> mid March!
+        Launching a new design for <a href="https://flareapp.io" target="_blank" class="underline">Flare</a> in April!
     </div>
 </div>
