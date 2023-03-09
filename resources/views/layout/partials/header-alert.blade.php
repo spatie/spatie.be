5750c98e18160ede133adb78ef9b65b466ca9812
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-03-09 10:36:42 +0100

    link header to blogpost

--- resources/views/layout/partials/header-alert.blade.php
+++ resources/views/layout/partials/header-alert.blade.php
@@ -1,5 +1,5 @@
 <div class="gradient gradient-pink text-white items-center justify-center p-2 text-center text-sm">
     <div class="mt-px ml-1">
-        We are launching a redesign for <a href="https://flareapp.io" target="_blank" class="underline">Flare</a> in April
+        We are launching a redesign for <a href="https://flareapp.io/blog/48-a-preview-of-our-upcoming-redesign" target="_blank" class="underline">Flare</a> in April
     </div>
 </div>
