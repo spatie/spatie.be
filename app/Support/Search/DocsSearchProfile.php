507da60ca57ec5a2b47ac42a7326992e2888bc62
Freek Van der Herten <freek@spatie.be>
2023-04-13 12:55:16 +0200

    wip

--- app/Support/Search/DocsSearchProfile.php
+++ app/Support/Search/DocsSearchProfile.php
@@ -10,6 +10,7 @@ class DocsSearchProfile extends DefaultSearchProfile
 {
     public function shouldIndex(UriInterface $url, ResponseInterface $response): bool
     {
+        info('should index ' . $url);
         return true;
     }
 }
