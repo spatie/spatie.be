1abe21ff2db73ac4a89512a7aafcd2c34caad06e
Chris Brown <code@drbyte.dev>
2023-04-28 19:22:12 -0400

    update Laravel-Permission branches
    Temporarily hiding v6(main) below v5 until v6 is actually released. Will re-order numerically at that time.

--- config/docs.php
+++ config/docs.php
@@ -232,7 +232,8 @@ return [
             "name" => "laravel-permission",
             "repository" => "spatie/laravel-permission",
             "branches" => [
-                "main" => "v5",
+                "v5" => "v5",
+                "main" => "v6",
                 "v4" => "v4",
                 "v3" => "v3",
             ],
