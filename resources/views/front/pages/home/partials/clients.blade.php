535e249750c7a3e506733da05cb8856a66646744
Freek Van der Herten <freek@spatie.be>
2023-08-11 14:32:24 +0200

    remove mutsy

--- resources/views/front/pages/home/partials/clients.blade.php
+++ resources/views/front/pages/home/partials/clients.blade.php
@@ -5,7 +5,6 @@
         </h2>
     </div>
     <div class="wrap grid grid-cols-2 mt-8 col-gap-8 row-gap-2 | md:grid-cols-4 md:col-gap-16">
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://mutsy.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/mutsy') }}</a></div>
         <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://krauthammer.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/krauthammer') }}</a></div>
         <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://vlaanderen.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vlaanderen') }}</a></div>
         <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://flux50.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/flux50') }}</a></div>
