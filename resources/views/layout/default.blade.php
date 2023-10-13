c0ded2bc59619111985cf7ede0b0ab6ae413de08
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-10-13 14:52:27 +0200

    upgrade laravel-comments

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -125,11 +125,9 @@
         });
     </script>
     <script defer src="https://unpkg.com/@alpinejs/focus@3.10.5/dist/cdn.min.js"></script>
-    <script defer src="https://unpkg.com/alpinejs@3.10.5/dist/cdn.min.js"></script>
 
     {!! schema()->localBusiness() !!}
 
-    <x-comments::scripts />
     @stack('modals')
 </body>
 </html>
