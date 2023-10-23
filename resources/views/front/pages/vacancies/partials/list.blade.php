d2c71b37398c023a4de634393a80ecc408cde218
sebastiandedeyne <sebastiandedeyne@gmail.com>
2023-10-23 16:12:42 +0200

    Remove old vacancies

--- resources/views/front/pages/vacancies/partials/list.blade.php
+++ resources/views/front/pages/vacancies/partials/list.blade.php
@@ -4,27 +4,6 @@
 </p>
 
 <div class="mt-4">
-    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'digital-designer') }}">Digital Designer</a>
-    <div class="text-xs text-gray">Full-time / Partially remote / Dutch</div>
-</div>
-{{-- <div class="mt-4">
-    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'react-engineer') }}">React Engineer</a>
-    <div class="text-xs text-gray">React with a hint of Vanilla JS</div>
-    <div class="text-xs text-gray">Full-time / Partially remote</div>
-</div> --}}
-{{-- <div class="mt-4">
-    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'digital-marketeer') }}">Digital Marketeer</a>
-    <div class="text-xs text-gray">Promoting our portfolio</div>
-    <div class="text-xs text-gray">Part-time / Fully or partially remote</div>
-</div> --}}
-{{--
-<div class="mt-4">
-    <a class="link-underline link-blue" href="{{ route('vacancies.show', 'backend-engineer') }}">Backend Engineer</a>
-    <div class="text-xs text-gray">PHP and Laravel</div>
-    <div class="text-xs text-gray">Full-time / Partially remote</div>
-</div>
---}}
-<div class="mt-4">
     <a class="link-underline link-blue" href="{{ route('vacancies.show', 'spontaneous-application') }}">Spontaneous Application</a>
     <div class="text-xs text-gray">Full-time / Partially remote</div>
 </div>
