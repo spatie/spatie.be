f17c6bc186296e38db1a325b5f5008c5d1ec5492
Alex Vanderbist <alex.vanderbist@gmail.com>
2023-11-06 14:53:07 +0100

    Center align client logos on homepage grid

--- resources/views/front/pages/home/partials/clients.blade.php
+++ resources/views/front/pages/home/partials/clients.blade.php
@@ -5,13 +5,13 @@
         </h2>
     </div>
     <div class="wrap grid grid-cols-2 mt-8 col-gap-8 row-gap-2 | md:grid-cols-4 md:col-gap-16">
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://krauthammer.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/krauthammer') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://vlaanderen.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vlaanderen') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://flux50.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/flux50') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://skylineeurope.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/skyline') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://bodartservicehouse.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/bodart') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://vluchtelingenwerk.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vluchtelingenwerk') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://vrijwilligerswerk.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vrijwilligerswerk') }}</a></div>
-        <div class="text-center"><a class="w-full max-w-logoclient inline-block" href="https://stmpdrcrds.com/images" target="_blank" rel="noreferrer noopener">{{ svg('clients/stmpd') }}</a></div>
+        <div class="flex items-center justify-center"><a class="w-full max-w-logoclient inline-block" href="https://krauthammer.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/krauthammer') }}</a></div>
+        <div class="flex items-center justify-center"><a class="w-full max-w-logoclient inline-block" href="https://vlaanderen.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vlaanderen') }}</a></div>
+        <div class="flex items-center justify-center"><a class="w-full max-w-logoclient inline-block" href="https://flux50.com" target="_blank" rel="noreferrer noopener">{{ svg('clients/flux50') }}</a></div>
+        <div class="flex items-center justify-center"><a class="w-full max-w-logoclient inline-block" href="https://skylineeurope.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/skyline') }}</a></div>
+        <div class="flex items-center justify-center"><a class="w-full max-w-logoclient inline-block" href="https://bodartservicehouse.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/bodart') }}</a></div>
+        <div class="flex items-center justify-center"><a class="w-full max-w-logoclient inline-block" href="https://vluchtelingenwerk.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vluchtelingenwerk') }}</a></div>
+        <div class="flex items-center justify-center"><a class="w-full max-w-logoclient inline-block" href="https://vrijwilligerswerk.be" target="_blank" rel="noreferrer noopener">{{ svg('clients/vrijwilligerswerk') }}</a></div>
+        <div class="flex items-center justify-center"><a class="w-full max-w-logoclient inline-block" href="https://stmpdrcrds.com/images" target="_blank" rel="noreferrer noopener">{{ svg('clients/stmpd') }}</a></div>
     </div>
 </section>
