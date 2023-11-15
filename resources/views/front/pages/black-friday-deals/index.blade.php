3f0ab0f0848f2e44ed5f107ecc34e189d6c2efd5
sebastiandedeyne <sebastiandedeyne@gmail.com>
2023-11-15 14:39:23 +0100

    Add Black Friday Deals page

---
+++ resources/views/front/pages/black-friday-deals/index.blade.php
@@ -0,0 +1,19 @@
+<x-page
+    ogImage="https://spatie.be/images/og-store.png"
+    title="Applications and digital courses built for modern developers"
+    background="/backgrounds/product.jpg"
+    description="Welcome in our store, by artisans for artisans. Get access to our paid products, courses and ebooks"
+>
+    <div class="section section-group">
+        <section class="section overflow-visible">
+            <div class="wrap">
+                <h2 class="title line-after mb-12">Public Service Announcement: Black Friday Deals</h2>
+                <iframe style="aspect-ratio: 16 / 9" class="w-full"
+                        src="https://www.youtube.com/embed/KMD8oP_9Z7U" title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+            </div>
+        </section>
+    </div>
+</x-page>
