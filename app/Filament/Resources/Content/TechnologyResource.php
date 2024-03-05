9b971dbf31064f317656f7c0c0107c37dfba9ef4
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-03-05 10:50:02 +0100

    Add technology resource to filament.

---
+++ app/Filament/Resources/Content/TechnologyResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources\Content;
+
+use App\Filament\Resources\Content\TechnologyResource\Pages;
+use App\Models\Technology;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Table;
+
+class TechnologyResource extends Resource
+{
+    protected static ?string $model = Technology::class;
+
+    protected static ?string $navigationGroup = 'Content';
+
+    protected static ?int $navigationSort = 6;
+
+    protected static ?string $navigationIcon = 'heroicon-o-heart';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('type')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('website_url')->searchable()->sortable(),
+                SpatieMediaLibraryImageColumn::make('avatar'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTechnologies::route('/'),
+            'create' => Pages\CreateTechnology::route('/create'),
+            'edit' => Pages\EditTechnology::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/Content/TechnologyResource/Pages/CreateTechnology.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\Content\TechnologyResource\Pages;
+
+use App\Filament\Resources\Content\TechnologyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTechnology extends CreateRecord
+{
+    protected static string $resource = TechnologyResource::class;
+}
