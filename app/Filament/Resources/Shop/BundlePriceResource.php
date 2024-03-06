8cba6af5a368b8b67f26cbe1c4550b4e58fc2887
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-03-06 14:46:21 +0100

    Configure bundle price form.

--- app/Filament/Resources/Shop/BundlePriceResource.php
+++ app/Filament/Resources/Shop/BundlePriceResource.php
@@ -2,12 +2,16 @@
 
 namespace App\Filament\Resources\Shop;
 
+use App\Domain\Shop\Models\Bundle;
 use App\Domain\Shop\Models\BundlePrice;
 use App\Domain\Shop\Models\PurchasablePrice;
 use App\Filament\Resources\Shop\BundlePriceResource\Pages;
 use App\Filament\Tables\Columns\BooleanColumn;
 use App\Filament\Tables\Columns\ResourceLinkColumn;
 use App\Support\Paddle\PaddleCountries;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -27,7 +31,26 @@ class BundlePriceResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Select::make('bundle_id')
+                    ->relationship(name: 'bundle')
+                    ->getOptionLabelFromRecordUsing(fn (Bundle $record) => $record->title)
+                    ->columnStart(1),
+
+                TextInput::make('country_code')
+                    ->columnStart(1)
+                    ->disabled(),
+
+                TextInput::make('currency_code')
+                    ->columnStart(1)
+                    ->disabled(),
+
+                TextInput::make('amount')
+                    ->integer()
+                    ->required()
+                    ->columnStart(1),
+
+                Toggle::make('overridden')
+                    ->columnStart(1),
             ]);
     }
 
@@ -36,8 +59,8 @@ class BundlePriceResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('id')->searchable()->sortable(),
-                ResourceLinkColumn::make('bundle.title', fn (PurchasablePrice $record) => route('filament.admin.resources.shop.purchasables.edit', $record->purchasable)),
-                Tables\Columns\TextColumn::make('country')->state(fn (PurchasablePrice $record) => PaddleCountries::getNameForCode($record->country_code) . "($record->country_code)")->searchable()->sortable(),
+                ResourceLinkColumn::make('bundle.title', fn (BundlePrice $record) => route('filament.admin.resources.shop.bundles.edit', $record->bundle)),
+                Tables\Columns\TextColumn::make('country')->state(fn (BundlePrice $record) => PaddleCountries::getNameForCode($record->country_code) . "($record->country_code)")->searchable()->sortable(),
                 Tables\Columns\TextColumn::make('currency_code')->searchable()->sortable(),
                 Tables\Columns\TextColumn::make('price_in_usd_cents')->sortable(),
                 BooleanColumn::make('overridden')->sortable(),
