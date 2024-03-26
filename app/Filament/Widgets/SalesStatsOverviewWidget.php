e7b3add84b3ea74f35cefa4e0f5834fbab837763
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-03-26 16:03:18 +0100

    Use new SimpleStat package.

--- app/Filament/Widgets/SalesStatsOverviewWidget.php
+++ app/Filament/Widgets/SalesStatsOverviewWidget.php
@@ -2,18 +2,19 @@
 
 namespace App\Filament\Widgets;
 
-use App\Domain\Shop\Models\Purchase;
-use App\Filament\SimpleStat;
 use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Spatie\FilamentSimpleStats\SimpleStat;
 
 class SalesStatsOverviewWidget extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            SimpleStat::createdInPastDays(User::class),
-            SimpleStat::createdInPastDays(Purchase::class),
+            SimpleStat::make(User::class)->last30Days()->dailyCount(),
+//            SimpleStat::make(User::class)->last30Days()->dailyAverage(),
+//            SimpleStat::make(User::class)->last30Days()->hourlyAverage(),
+//            SimpleStat::createdInPastDays(Purchase::class),
         ];
     }
 }
