cc37858f3cbbaf120ea4326760679789d65a522a
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-03-29 16:50:04 +0100

    Restore docs in config

--- config/docs.php
+++ config/docs.php
@@ -2,7 +2,64 @@
 
 return [
     'repositories' => [
+        [
+            "name" => "laravel-pdf",
+            "repository" => "spatie/laravel-pdf",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "lighthouse-php",
+            "repository" => "spatie/lighthouse-php",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-prometheus",
+            "repository" => "spatie/laravel-prometheus",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-open-telemetry",
+            "repository" => "spatie/laravel-open-telemetry",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-dynamic-servers",
+            "repository" => "spatie/laravel-dynamic-servers",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-livewire-wizard",
+            "repository" => "spatie/laravel-livewire-wizard",
+            "branches" => [
+                "main" => "v2",
+                "v1" => "v1",
 
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-translatable",
+            "repository" => "spatie/laravel-translatable",
+            "branches" => [
+                "main" => "v6",
+            ],
+            "category" => "Laravel",
+        ],
         [
             "name" => "browsershot",
             "repository" => "spatie/browsershot",
@@ -12,6 +69,289 @@ return [
 
             ],
             "category" => "General PHP",
+        ],
+        [
+            "name" => "laravel-route-discovery",
+            "repository" => "spatie/laravel-route-discovery",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-comments",
+            "repository" => "spatie/laravel-comments",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-health",
+            "repository" => "spatie/laravel-health",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-site-search",
+            "repository" => "spatie/laravel-site-search",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-markdown",
+            "repository" => "spatie/laravel-markdown",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "ray",
+            "repository" => "spatie/ray",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "General PHP",
+        ],
+        [
+            "name" => "laravel-backup",
+            "repository" => "spatie/laravel-backup",
+            "branches" => [
+                "main" => "v8",
+                "v7" => "v7",
+                "v6" => "v6",
+                "v5" => "v5",
+                "v4" => "v4",
+                "v3" => "v3",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-medialibrary",
+            "repository" => "spatie/laravel-medialibrary",
+            "branches" => [
+                "main" => "v11",
+                "v10" => "v10",
+                "v9" => "v9",
+                "v8" => "v8",
+                "v7" => "v7",
+                "v6" => "v6",
+                "v5" => "v5",
+                "v4" => "v4",
+                "v3" => "v3",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-blade-x",
+            "repository" => "spatie/laravel-blade-x",
+            "branches" => [
+                "master" => "v2",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-tags",
+            "repository" => "spatie/laravel-tags",
+            "branches" => [
+                "main" => "v4",
+                "v3" => "v3",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-activitylog",
+            "repository" => "spatie/laravel-activitylog",
+            "branches" => [
+                "main" => "v4",
+                "v3" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-html",
+            "repository" => "spatie/laravel-html",
+            "branches" => [
+                "main" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-server-monitor",
+            "repository" => "spatie/laravel-server-monitor",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-uptime-monitor",
+            "repository" => "spatie/laravel-uptime-monitor",
+            "branches" => [
+                "main" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-slack-slash-command",
+            "repository" => "spatie/laravel-slack-slash-command",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "menu",
+            "repository" => "spatie/menu",
+            "branches" => [
+                "main" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "General PHP",
+        ],
+        [
+            "name" => "image",
+            "repository" => "spatie/image",
+            "branches" => [
+                "main" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "General PHP",
+        ],
+        [
+            "name" => "enum",
+            "repository" => "spatie/enum",
+            "branches" => [
+                "main" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "General PHP",
+        ],
+        [
+            "name" => "laravel-model-states",
+            "repository" => "spatie/laravel-model-states",
+            "branches" => [
+                "main" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-permission",
+            "repository" => "spatie/laravel-permission",
+            "branches" => [
+                "main" => "v6",
+                "v5" => "v5",
+                "v4" => "v4",
+                "v3" => "v3",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-query-builder",
+            "repository" => "spatie/laravel-query-builder",
+            "branches" => [
+                "main" => "v5",
+                "v4" => "v4",
+                "v3" => "v3",
+                "v2" => "v2",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-event-sourcing",
+            "repository" => "spatie/laravel-event-sourcing",
+            "branches" => [
+                "main" => "v7",
+                "v6" => "v6",
+                "v5" => "v5",
+                "v4" => "v4",
+                "v3" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-backup-server",
+            "repository" => "spatie/laravel-backup-server",
+            "branches" => [
+                "main" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-dashboard",
+            "repository" => "spatie/laravel-dashboard",
+            "branches" => [
+                "main" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-multitenancy",
+            "repository" => "spatie/laravel-multitenancy",
+            "branches" => [
+                "main" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "typescript-transformer",
+            "repository" => "spatie/typescript-transformer",
+            "branches" => [
+                "main" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "General PHP",
+        ],
+        [
+            "name" => "laravel-data",
+            "repository" => "spatie/laravel-data",
+            "branches" => [
+                "main" => "v4",
+                "v3" => "v3",
+                "v2" => "v2",
+                "v1" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "laravel-notification-log",
+            "repository" => "spatie/laravel-notification-log",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Laravel",
+        ],
+        [
+            "name" => "wordpress-mailcoach",
+            "repository" => "spatie/wordpress-mailcoach",
+            "branches" => [
+                "main" => "v1",
+            ],
+            "category" => "Other",
         ],
     ],
 ];
