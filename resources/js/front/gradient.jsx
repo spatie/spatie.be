7bfad0a603ee822247c259969ca56ff3a08e1798
Rias <hey@rias.be>
2024-04-18 10:31:24 +0200

    Add wire:navigate to links

--- resources/js/front/gradient.jsx
+++ resources/js/front/gradient.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import { ShaderGradientCanvas, ShaderGradient } from 'shadergradient';
 
-const root = createRoot(document.getElementById("gradient"));
-const url = document.getElementById("gradient").dataset.url;
+document.addEventListener('livewire:navigated', () => {
+    const root = createRoot(document.getElementById("gradient"));
+    const url = document.getElementById("gradient").dataset.url;
 
-root.render(
-    <ShaderGradientCanvas>
-        <ShaderGradient
-            control='query'
-            urlString={url}
-            enableTransition={false}
-        />
-    </ShaderGradientCanvas>
-);
+    root.render(
+        <ShaderGradientCanvas>
+            <ShaderGradient
+                control='query'
+                urlString={url}
+                enableTransition={false}
+            />
+        </ShaderGradientCanvas>
+    );
+});
--- resources/views/front/pages/open-source/components/menu.blade.php
+++ resources/views/front/pages/open-source/components/menu.blade.php
@@ -6,19 +6,19 @@
     <aside class="w-full max-w-[360px] flex-shrink-0 md:pl-32 flex flex-col md:justify-end md:items-end -mb-8 pt-10 md:pt-0 border-t md:border-t-0 md:border-l border-white/20">
         <ul class="text-xl w-full sm:max-w-[240px]">
             <li class="mb-3 {{ Route::is('open-source.index') ? 'font-bold' : 'text-white/50' }} hover:text-white transition-colors">
-                <a href="{{ route('open-source.index') }}" class="flex items-center justify-between">
+                <a wire:navigate.hover href="{{ route('open-source.index') }}" class="flex items-center justify-between">
                     <span>Open Source</span>
                     <svg class="fill-current w-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 7 12"><path d="m6.687 6-.53.53-4.5 4.5-.532.532L.063 10.5l.53-.53L4.563 6 .596 2.03.063 1.5 1.125.438l.53.53 4.5 4.5.532.532Z"/></svg>
                 </a>
             </li>
             <li class="mb-3 {{ Route::is('open-source.packages') ? 'font-bold' : 'text-white/50' }} hover:text-white transition-colors">
-                <a href="{{ route('open-source.packages') }}" class="flex items-center justify-between">
+                <a wire:navigate.hover href="{{ route('open-source.packages') }}" class="flex items-center justify-between">
                     <span>Packages</span>
                     <svg class="fill-current w-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 7 12"><path d="m6.687 6-.53.53-4.5 4.5-.532.532L.063 10.5l.53-.53L4.563 6 .596 2.03.063 1.5 1.125.438l.53.53 4.5 4.5.532.532Z"/></svg>
                 </a>
             </li>
             <li class=" {{ Route::is('open-source.postcards') ? 'font-bold' : 'text-white/50' }} hover:text-white transition-colors">
-                <a href="{{ route('open-source.postcards') }}" class="flex items-center justify-between">
+                <a wire:navigate.hover href="{{ route('open-source.postcards') }}" class="flex items-center justify-between">
                     <span>Postcards</span>
                     <svg class="fill-current w-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 7 12"><path d="m6.687 6-.53.53-4.5 4.5-.532.532L.063 10.5l.53-.53L4.563 6 .596 2.03.063 1.5 1.125.438l.53.53 4.5 4.5.532.532Z"/></svg>
                 </a>
