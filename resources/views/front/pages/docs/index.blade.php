18dfe413c59564acb003e3fc943571b5252b9d9b
Rias <hey@rias.be>
2024-04-22 12:49:18 +0200

    Add fade in to docs gradient

--- resources/views/front/pages/docs/index.blade.php
+++ resources/views/front/pages/docs/index.blade.php
@@ -5,7 +5,14 @@
         <style>html { font-size: 16px !important; }</style>
     @endpush
     @push('startBody')
-        <div class="absolute top-0 left-0 right-0 z-0 pointer-events-none">
+        <div
+            x-show="show"
+            x-data="{ show: false }"
+            x-init="setTimeout(() => show = true, 50)"
+            x-transition.opacity.duration.1000ms
+            x-cloak
+            class="absolute top-0 left-0 right-0 z-0 pointer-events-none"
+        >
             <div id="gradient" class="aspect-[9/16] sm:aspect-[1440/700] w-full" data-url="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.8&cAzimuthAngle=180&cDistance=2&cPolarAngle=80&cameraZoom=9.1&color1=%23197593&color2=%23328c7d&color3=%23EAE8E5&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=20&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=1&positionY=-0.8&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=50&rotationY=0&rotationZ=-60&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=2&uFrequency=0&uSpeed=0.05&uStrength=0.6&uTime=8&wireframe=false"></div>
             <div class="absolute inset-0 z-10 w-full h-full aspect-[9/16] sm:aspect-[1440/700] bg-gradient-to-b from-transparent to-oss-gray"></div>
         </div>
