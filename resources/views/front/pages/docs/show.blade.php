5301d5f8a8b86ed031ca7faeeef0fe304fe4a5d5
sebastiandedeyne <sebastiandedeyne@gmail.com>
2024-05-16 13:16:01 +0200

    Add wire:navigate.hover to next & previous links in docs

--- resources/views/front/pages/docs/show.blade.php
+++ resources/views/front/pages/docs/show.blade.php
@@ -104,7 +104,7 @@
                 <div class="border border-gray/25 p-6 rounded-md mt-10 flex justify-between items-center bg-link-card-light">
                     <div class="w-full">
                         @if($prevPage)
-                            <a class="flex items-center gap-x-2 text-blue hover:underline" href="{{ $prevPage->url }}">
+                            <a class="flex items-center gap-x-2 text-blue hover:underline" href="{{ $prevPage->url }}" wire:navigate.hover>
                                 <svg class="w-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 7 12"><path fill="#197593" d="m.313 5.623.53.53 4.5 4.5.531.532 1.062-1.062-.53-.53-3.97-3.97 3.968-3.97.532-.53L5.874.062l-.53.532-4.5 4.5-.531.53Z"/></svg>
                                 <span class="leading-none mb-px">{{ $prevPage->title }}</span>
                             </a>
@@ -113,7 +113,7 @@
                     <div class="h-5 w-px bg-oss-gray-dark"></div>
                     <div class="w-full flex justify-end">
                         @if($nextPage)
-                            <a class="flex items-center gap-x-2 text-blue hover:underline" href="{{ $nextPage->url }}">
+                            <a class="flex items-center gap-x-2 text-blue hover:underline" href="{{ $nextPage->url }}" wire:navigate.hover>
                                 <span class="leading-none mb-px">{{ $nextPage->title }}</span>
                                 <svg class="w-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 7 12"><path fill="#197593" d="m6.686 5.623-.53.53-4.5 4.5-.532.532-1.062-1.062.53-.53 3.97-3.97-3.967-3.97-.532-.53L1.123.062l.53.53 4.5 4.5.532.531Z"/></svg>
                             </a>
