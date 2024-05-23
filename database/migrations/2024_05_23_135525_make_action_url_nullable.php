ae8f8bbf42b95045461b2506a862c40d7845d6b2
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-05-23 13:56:49 +0200

    Make action_url on products nullable

---
+++ database/migrations/2024_05_23_135525_make_action_url_nullable.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('action_url')->nullable()->change();
+        });
+    }
+};
