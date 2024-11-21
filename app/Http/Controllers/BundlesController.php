50a7d2317b2c8fced3425a103904913871ad280f
Rias <hey@rias.be>
2024-11-21 12:50:09 +0100

    404 on invisble bundles

--- app/Http/Controllers/BundlesController.php
+++ app/Http/Controllers/BundlesController.php
@@ -9,7 +9,10 @@ class BundlesController
 {
     public function show(Bundle $bundle): View
     {
+        abort_unless($bundle->visible || current_user()?->hasAccessToUnReleasedProducts(), 404);
+
         $payLink = null;
+
         if (current_user()) {
             $payLink = current_user()->getPayLinkForBundle($bundle);
         }
