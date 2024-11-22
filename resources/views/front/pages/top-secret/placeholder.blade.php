9b7a880ec8da0b0f3b340455fcc31c8ef12e15e4
Jimi Robaer <jimi.robaer@hey.com>
2024-11-22 14:24:46 +0100

    Add placeholder template

---
+++ resources/views/front/pages/top-secret/placeholder.blade.php
@@ -0,0 +1,3 @@
+<div class="bg-bf-dark-gray bg-cover" style="background-image:url('https://content.spatie.be/assets/24-bf-video-loop-cover-2.jpeg')">
+    <video class="w-screen h-screen" src="https://content.spatie.be/assets/24-bf-video-loop.mp4" autoplay loop muted playsinline></video>
+</div>
