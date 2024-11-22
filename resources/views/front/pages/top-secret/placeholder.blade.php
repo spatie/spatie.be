734174601336bce68c57ebb173fa541bf69379f6
Alex Vanderbist <alex.vanderbist@gmail.com>
2024-11-22 15:28:17 +0100

    Add page title

--- resources/views/front/pages/top-secret/placeholder.blade.php
+++ resources/views/front/pages/top-secret/placeholder.blade.php
@@ -1,4 +1,4 @@
-<x-blank>
+<x-blank title="S.P.A.T.I.E">
     <div class="bg-bf-dark-gray bg-cover"
          style="background-image:url('https://content.spatie.be/assets/24-bf-video-loop-cover-2.jpeg')">
         <video class="w-screen h-screen" src="https://content.spatie.be/assets/24-bf-video-loop.mp4" autoplay loop muted
