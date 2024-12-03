8d66ad08c80bebea127abb9e9747ac4a6b83d8ba
Jimi Robaer <jimi.robaer@hey.com>
2024-12-03 11:57:39 +0100

    Revert "Revert "the not so secret black friday commit which needs to be reversed""
    This reverts commit f47cef4ebafb20a45f383041c6263abd3c964afe.

--- resources/views/front/pages/home/index.blade.php
+++ resources/views/front/pages/home/index.blade.php
@@ -1,34 +1,22 @@
-@php
-    $image = image("/backgrounds/bf-24-hero.jpg");
-@endphp
-
-@push('startBody')
-    <div class="wallpaper">
-        <img srcset="{{ $image->getSrcset() }}" src="{{ $image->getUrl() }}" width="2400" sizes="100vw" alt="" class="h-svh object-cover">
-    </div>
-@endpush
-
 <x-page
-        title="Websites & webapplications in Laravel"
-        background=""
-        bodyClass="bg-bf-dark-gray">
+    title="Websites & webapplications in Laravel"
+    background="/backgrounds/home-2020.jpg">
     <x-slot name="description">
         Spatie is a digital allrounder: we design solid websites & web applications using Laravel & Vue. No frills, just
         proven expertise. From Antwerp, Belgium
     </x-slot>
 
-    {{-- @include('front.pages.home.partials.banner') --}}
-    @include('front.pages.home.partials.bf-banner')
+    @include('front.pages.home.partials.banner')
 
     <div class="mb-8">
         <a href="{{ route('products.index') }}">
-        {{-- @include('front.pages.products.partials.ctaLaraconEU') --}}
+            {{-- @include('front.pages.products.partials.ctaLaraconEU') --}}
         </a>
     </div>
 
-    {{-- @include('front.pages.home.partials.news') --}}
+    @include('front.pages.home.partials.news')
 
-    <div class="section section-group section-fade bg-white">
+    <div class="section section-group section-fade">
         @include('front.pages.home.partials.portfolio')
         @include('front.pages.home.partials.newsletter')
         @include('front.pages.home.partials.clients')
