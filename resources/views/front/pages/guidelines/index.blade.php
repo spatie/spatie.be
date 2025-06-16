6e8f96b57497e0823e4829a3d8731278ef91d8a8
Nick Bevers <nick@spatie.be>
2025-06-16 11:19:16 +0200

    feat: wip for guidleines page

--- resources/views/front/pages/guidelines/index.blade.php
+++ resources/views/front/pages/guidelines/index.blade.php
@@ -1,39 +1,34 @@
-<x-page title="Guidelines" background="/backgrounds/guidelines.jpg">
+<x-page title="Guidelines" background="/backgrounds/blog-index.jpg" body-class="bg-oss-gray" main-class="font-pt text-oss-royal-blue font-medium text-18 leading-140 antialiased">
     <x-slot name="description">
         A set of guidelines we use to bring our projects to a good end.
         Consistency is the key to writing maintainable software.
     </x-slot>
 
-    <section id="banner" class="banner" role="banner">
-        <div class="wrap">
-            <h1 class="banner-slogan">
-                Guidelines
-            </h1>
-            <p class="banner-intro">
-                Consistency is key
-            </p>
-        </div>
-    </section>
+    <header class="wrapper-lg px-7 sm:px-16 mt-4 lg:mt-12 md:mb-16">
+        <x-headers.super class="md:text-[96px] md:text-center text-white drop-shadow-2xl">
+            Coding <br> Guidelines
+        </x-headers.super>
+    </header>
 
     <section>
-        <div class="wrap wrap-6 mb-16">
-            <div class="sm:col-span-6 max-w-lg">
+        <div class="wrapper-lg px-7 sm:px-16 mt-8">
+            <div class="sm:col-span-6 max-w-screen-sm mx-auto">
                 <div class="markup links-underline links-blue">
                     <p class="text-lg">
-                        Most projects are not built or maintained by a single person. Instead, there is a collection of
-                        people involved, who all have their own personal preferences. If everyone would use their
-                        personal style, maintainability would suffer.
+                        Most projects are not created or maintained by one person. 
+                        Instead, a group of people work together, each with their own preferences. 
+                        If everyone used their own style, projects would be difficult to maintain.
                     </p>
                     <p class="text-lg">
-                        The members of our team regularly discuss the pros and cons of each of their
-                        individual programming preferences. When reaching a consensus, we write it down in these
-                        guidelines, together with the reasons why a certain approach was picked.
+                        Our team often discusses the pros and cons of our different programming styles. 
+                        When we agree on something, we write it down in these guidelines and explain why we chose that approach.
                     </p>
 
                     <p class="text-lg">
-                        We consider our guidelines as a living document. People, teams, and opinions change over time.
-                        We don't dogmatically keep following rules we agreed on in the past, but keep challenging them.
-                        New experiences lead to new and improved guidelines.
+                        We like to think of our guidelines as a living document. 
+                        People, teams and opinions change over time. 
+                        We don't adhere strictly to old rules, but constantly challenge them. 
+                        New experiences lead to better guidelines.
                     </p>
                     <div class="mt-12">
                     @include('front.pages.guidelines.partials.writing-readable-php-cta')
--- resources/views/front/pages/guidelines/partials/page.blade.php
+++ resources/views/front/pages/guidelines/partials/page.blade.php
@@ -1,6 +1,7 @@
-<div class="line-l">
-    <a href="{{ action([\App\Http\Controllers\GuidelinesController::class, 'show'], $page->slug) }}">
-        <h2 class="title-sm link-black link-underline">{{ $page->title }}</h2>
+<div class="">
+    <a href="{{ action([\App\Http\Controllers\GuidelinesController::class, 'show'], $page->slug) }}" class="flex flex-col justify-center items-center">
+        {{ image('guidelines/' . $page->slug . '.png') }}
+        <h2 class="text-oss-royal-blue text-bold">{{ $page->title }}</h2>
         <p class="mt-4">{{ $page->description }}</p>
     </a>
 </div>
