f417c6a4a86608985e4f8f33e1807ad0e69d434d
Freek Van der Herten <freek@spatie.be>
2025-09-23 11:49:24 +0200

    improve results

--- app/Support/Transformers/LdJsonTransformer.php
+++ app/Support/Transformers/LdJsonTransformer.php
@@ -24,6 +24,6 @@ class LdJsonTransformer extends BaseLdJsonTransformer
     {
         $content = Str::limit(strip_tags($this->urlContent), 6000);
 
-        return "Summarize the following webpage to ld+json. Only return valid json, no backtick openings. Make the snippet as complete as possible. Do not add 'potentialAction' key or type 'searchAction'. This is the content that we fetched for url  {$this->url}: {$content}";
+        return "Summarize the following webpage to ld+json. Only return valid json, no backtick openings. Make the snippet as complete as possible. Do not add 'potentialAction' key or type 'searchAction'.  Do not make up content. This is our contact information. Email: info@spatie.be, Telephone: +32 3 292 56 79, Address: Kruikstraat 22, Box 12, 2018 Antwerp, Belgium. This is the content that we fetched for url {$this->url}: {$content}";
     }
 }
