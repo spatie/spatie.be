43aa6b6b8e37c99e9c176f1cb70e3a850864df9c
Freek Van der Herten <freek@spatie.be>
2025-11-13 09:40:15 +0100

    wip

--- app/Providers/AiTransformerServiceProvider.php
+++ app/Providers/AiTransformerServiceProvider.php
@@ -2,14 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\Crawler\Crawler;
 use App\Support\Transformers\LdJsonTransformer;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\UriInterface;
-use Spatie\Crawler\Crawler;
-use Spatie\Crawler\CrawlObservers\CrawlObserver;
-use Spatie\Crawler\CrawlProfiles\CrawlInternalUrls;
+
 use Spatie\LaravelUrlAiTransformer\Support\Transform;
 
 class AiTransformerServiceProvider extends ServiceProvider
@@ -18,48 +14,4 @@ class AiTransformerServiceProvider extends ServiceProvider
     {
         Transform::urls('/', '/about-us')->usingTransformers(new LdJsonTransformer());
     }
-
-    /**
-     * @return array<int, string>
-     */
-    protected function crawlAllUrls(): array
-    {
-        $startUrl = url('/');
-
-        $urls = [];
-
-        Crawler::create()
-            ->setCrawlObserver(new class ($urls) extends CrawlObserver {
-                /** @param array<int, string> $urls  */
-                public function __construct(protected array &$urls)
-                {
-                }
-
-                public function crawled(
-                    UriInterface $url,
-                    ResponseInterface $response,
-                    ?UriInterface $foundOnUrl = null,
-                    ?string $linkText = null
-                ): void {
-                    $url = (string) $url;
-
-                    if (Str::endsWith($url, '.svg')) {
-                        return;
-                    }
-
-                    $this->urls[] = (string) $url;
-                }
-            })
-            ->setCrawlProfile(new CrawlInternalUrls($startUrl))
-            ->startCrawling($startUrl);
-
-        return array_unique($urls);
-    }
-
-
-
-
-    //        Transform::urls(
-    //            fn () => $this->crawlAllUrls()
-    //        )->usingTransformers(new LdJsonTransformer());
 }
---
+++ app/Services/Crawler/Crawler.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services\Crawler;
+
+use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Str;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\UriInterface;
+use Spatie\Crawler\Crawler as SpatieCrawler;
+use Spatie\Crawler\CrawlObservers\CrawlObserver;
+use Spatie\Crawler\CrawlProfiles\CrawlInternalUrls;
+
+class Crawler
+{
+    public static function getAllUrls(): array
+    {
+        $startUrl = url('/');
+
+        $urls = [];
+
+        SpatieCrawler::create([
+            RequestOptions::VERIFY => false,
+        ])
+            ->setCrawlObserver(new class ($urls) extends CrawlObserver {
+                /** @param array<int, string> $urls  */
+                public function __construct(protected array &$urls)
+                {
+                }
+
+                public function crawled(
+                    UriInterface $url,
+                    ResponseInterface $response,
+                    ?UriInterface $foundOnUrl = null,
+                    ?string $linkText = null
+                ): void {
+                    $url = (string) $url;
+
+                    if (Str::endsWith($url, '.svg')) {
+                        return;
+                    }
+
+                    $this->urls[] = (string) $url;
+                }
+            })
+            ->setCrawlProfile(new CrawlInternalUrls($startUrl))
+            ->startCrawling($startUrl);
+
+        return array_unique($urls);
+    }
+}
