ea27f098b054d64cd7aa40d5253e75cc326585ec
Jimi Robaer <jimi.robaer@hey.com>
2025-11-18 10:17:28 +0100

    Move BF updates behind flags

--- resources/views/front/pages/home/index.blade.php
+++ resources/views/front/pages/home/index.blade.php
@@ -1,31 +1,38 @@
 @php
+    $isBlackFriday = config('black-friday.enabled');
+
     $image = image("/backgrounds/bf-25-hero.jpg");
+    $sectionClasses = $isBlackFriday ? 'section section-group section-fade bg-white pt-32' : 'section section-group section-fade';
 @endphp
 
-@push('startBody')
-    <div class="wallpaper">
-        <img srcset="{{ $image->getSrcset() }}" src="{{ $image->getUrl() }}" width="2400" sizes="100vw" alt="" class="aspect-[2/3] md:h-svh object-cover">
-    </div>
-@endpush
+@if($isBlackFriday)
+    @push('startBody')
+        <div class="wallpaper">
+            <img srcset="{{ $image->getSrcset() }}" src="{{ $image->getUrl() }}" width="2400" sizes="100vw" alt="" class="aspect-[2/3] md:h-svh object-cover">
+        </div>
+    @endpush
+@endif
 
 <x-page
     title="Websites & web applications in Laravel"
+    :background="$isBlackFriday ? '' : '/backgrounds/home-2020.jpg'"
 >
     <x-slot name="description">
         Spatie is a digital allrounder: we design solid websites & web applications using Laravel & Vue. No frills, just
         proven expertise. From Antwerp, Belgium
     </x-slot>
 
-    {{-- @include('front.pages.home.partials.banner') --}}
-    @include('front.pages.home.partials.bf-banner')
+    @if($isBlackFriday)
+        @include('front.pages.home.partials.bf-banner')
+    @else
+        @include('front.pages.home.partials.banner')
+    @endif
 
-    <div class="mb-8">
-        <a href="{{ route('products.index') }}">
-            {{-- @include('front.pages.products.partials.ctaLaraconEU') --}}
-        </a>
-    </div>
+    @if(!$isBlackFriday)
+        @include('front.pages.home.partials.news')
+    @endif
 
-    <div class="section section-group section-fade bg-white">
+    <div class="{{ $sectionClasses }}">
         @include('front.pages.home.partials.portfolio')
         @include('front.pages.home.partials.newsletter')
         @include('front.pages.home.partials.clients')
