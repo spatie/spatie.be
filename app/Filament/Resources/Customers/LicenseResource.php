a5167a3b0d377e441f6f5f49de7f0f33bff4b327
Tim Van Dijck <tim@spatie.be>
2025-12-12 15:59:19 +0100

    Add columns to license table

--- app/Filament/Resources/Customers/LicenseResource.php
+++ app/Filament/Resources/Customers/LicenseResource.php
@@ -58,6 +58,18 @@ class LicenseResource extends Resource
         return $table
             ->columns([
                 LicensePurchasableNameColumn::make(),
+                TextColumn::make('assignment.purchase.receipt.order_id')
+                    ->searchable(),
+                ResourceLinkColumn::make('assignment.purchase.id',
+                    function (License $record) {
+                        if (! $record->assignment) {
+                            return null;
+                        }
+
+                        return route('filament.admin.resources.customers.purchase.edit', $record->assignment->purchase);
+                    })
+                    ->label('Purchase ID')
+                    ->searchable(),
                 ResourceLinkColumn::make(
                     'assignment.user.email',
                     function (License $record) {
