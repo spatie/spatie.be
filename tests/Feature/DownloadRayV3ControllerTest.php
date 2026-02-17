e8e4769c180218cf355d87e77282ee188c300b0e
Freek Van der Herten <freek@spatie.be>
2026-02-17 15:32:22 +0100

    l12

--- tests/Feature/DownloadRayV3ControllerTest.php
+++ tests/Feature/DownloadRayV3ControllerTest.php
@@ -4,28 +4,55 @@ use Illuminate\Support\Facades\Http;
 
 beforeEach(function () {
     Http::fake([
-        'https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/beta/darwin/universal/RELEASES.json' => Http::response([
-            'currentRelease' => '1.0.0',
-        ]),
+        'https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/latest-mac.yml' => Http::response(<<<'YML'
+version: 3.1.0
+files:
+  - url: ray-3.1.0-latest-darwin-arm64.dmg
+    sha512: abc123
+    size: 119851354
+  - url: ray-3.1.0-latest-darwin-x64.dmg
+    sha512: def456
+    size: 122240909
+YML),
+        'https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/latest.yml' => Http::response(<<<'YML'
+version: 3.1.0
+files:
+  - url: ray-3.1.0-latest-win32-x64-setup.exe
+    sha512: abc123
+    size: 102282264
+YML),
+        'https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/latest-linux.yml' => Http::response(<<<'YML'
+version: 3.1.0
+files:
+  - url: ray-3.1.0-latest-linux-x86_64.AppImage
+    sha512: abc123
+    size: 124330170
+YML),
     ]);
 });
 
-it('redirects macOS downloads to S3 domain', function () {
+it('redirects macOS arm64 downloads to S3 domain', function () {
     $this
-        ->get('/products/ray/v3/download/macos/latest')
-        ->assertRedirectContains('https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/beta/darwin/universal');
+        ->get('/products/ray/v3/download/macos-arm64/latest')
+        ->assertRedirectContains('https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/ray-3.1.0-latest-darwin-arm64.dmg');
+});
+
+it('redirects macOS x64 downloads to S3 domain', function () {
+    $this
+        ->get('/products/ray/v3/download/macos-x64/latest')
+        ->assertRedirectContains('https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/ray-3.1.0-latest-darwin-x64.dmg');
 });
 
 it('redirects Windows downloads to S3 domain', function () {
     $this
         ->get('/products/ray/v3/download/windows/latest')
-        ->assertRedirectContains('https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/beta/win32/x64');
+        ->assertRedirectContains('https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/ray-3.1.0-latest-win32-x64-setup.exe');
 });
 
 it('redirects Linux downloads to S3 domain', function () {
     $this
         ->get('/products/ray/v3/download/linux/latest')
-        ->assertRedirectContains('https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/beta/linux/x64');
+        ->assertRedirectContains('https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/ray-3.1.0-latest-linux-x86_64.AppImage');
 });
 
 it('returns 404 for invalid platforms', function () {
--- tests/Feature/GitHubAds/RandomizeAdOnGitHubRepositoriesJobTest.php
+++ tests/Feature/GitHubAds/RandomizeAdOnGitHubRepositoriesJobTest.php
@@ -3,47 +3,56 @@
 use App\Jobs\RandomizeAdsOnGitHubRepositoriesJob;
 use App\Models\Ad;
 use App\Models\Repository;
-use Illuminate\Support\Facades\Storage;
-
-beforeEach(function () {
-    Storage::fake();
-});
 
 it('can randomize ads on git hub repositories', function () {
-    Ad::factory()->count(10)->create();
+    $ads = collect([4, 5, 7, 9])->map(fn ($id) => Ad::factory()->create(['id' => $id]));
 
     $repositories = Repository::factory()->count(10)->create([
         'ad_should_be_randomized' => true,
+    ]);
+
+    dispatch(new RandomizeAdsOnGitHubRepositoriesJob());
+
+    $repositories->each(function (Repository $repository) use ($ads) {
+        expect($repository->refresh()->ad_id)->toBeIn($ads->pluck('id')->toArray());
+    });
+});
+
+it('will only use ads with the hardcoded ids', function () {
+    Ad::factory()->count(10)->create();
+
+    $targetAd = Ad::factory()->create(['id' => 4]);
+
+    $repositories = Repository::factory()->count(10)->create([
+        'ad_should_be_randomized' => true,
+    ]);
+
+    dispatch(new RandomizeAdsOnGitHubRepositoriesJob());
+
+    $repositories->each(function (Repository $repository) use ($targetAd) {
+        expect($repository->refresh()->ad_id)->toEqual($targetAd->id);
+    });
+});
+
+it('will not update a repository whose ad should not be randomized', function () {
+    collect([4, 5, 7, 9])->each(fn ($id) => Ad::factory()->create(['id' => $id]));
+
+    $repositories = Repository::factory()->count(10)->create([
+        'ad_should_be_randomized' => false,
     ]);
 
     dispatch(new RandomizeAdsOnGitHubRepositoriesJob());
 
     $repositories->each(function (Repository $repository) {
-        expect($repository->refresh()->ad)->not()->toBeNull();
-        Storage::disk('github_ads')->assertExists($repository->gitHubAdImagePath());
+        expect($repository->refresh()->ad_id)->toBeNull();
     });
 });
 
-it('will not use ads that are not active', function () {
-    Ad::factory()->inactive()->count(10)->create();
-
-    $activeAd = Ad::factory()->active()->create();
-    $repositories = Repository::factory()->count(10)->create([
-        'ad_should_be_randomized' => true,
-    ]);
-
-    dispatch(new RandomizeAdsOnGitHubRepositoriesJob());
-
-    $repositories->each(function (Repository $repository) use ($activeAd) {
-        expect($repository->refresh()->ad_id)->toEqual($activeAd->id);
-    });
-});
-
-it('will not update a repository whose ad should not be randomized', function () {
+it('does nothing when none of the hardcoded ad ids exist', function () {
     Ad::factory()->count(10)->create();
 
     $repositories = Repository::factory()->count(10)->create([
-        'ad_should_be_randomized' => false,
+        'ad_should_be_randomized' => true,
     ]);
 
     dispatch(new RandomizeAdsOnGitHubRepositoriesJob());
