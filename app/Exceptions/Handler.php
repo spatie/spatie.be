c8f2e3158571a95e8f0aaebc784309f4a9a55bd6
Tim Van Dijck <tim@spatie.be>
2026-04-08 16:53:49 +0200

    Add Flare again

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -17,5 +17,8 @@ class Handler extends ExceptionHandler
 
     public function register(): void
     {
+        $this->reportable(function (\Throwable $e) {
+            \Spatie\LaravelFlare\Facades\Flare::report($e);
+        });
     }
 }
