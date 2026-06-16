78487cc26d3658e14b1138916b1c1c2546813cdf
Tim Van Dijck <tim@spatie.be>
2026-06-16 14:20:03 +0200

    Update profile purchases overview (#425)
    * Update profile purchases overview
    
    * Fix purchases overview static analysis

--- app/Http/Controllers/PurchasesController.php
+++ app/Http/Controllers/PurchasesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Domain\Shop\Models\Purchase;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -10,27 +11,41 @@ class PurchasesController
 {
     public function __invoke(Request $request): View
     {
-        $courses = $request->user()
+        $user = $request->user();
+
+        $courses = $user
             ->assignmentsWithoutRenewals()
-            ->with(['purchasable.product', 'purchasable.media'])
+            ->with(['purchase.user', 'purchasable.product', 'purchasable.media'])
             ->whereHas(
                 'purchasable',
                 fn (Builder $query) => $query
-                ->whereHas('series')
-                ->orWhereHas('media', fn (Builder $query) => $query->where('collection_name', 'downloads'))
+                    ->whereHas('series')
+                    ->orWhereHas('media', fn (Builder $query) => $query->where('collection_name', 'downloads'))
             )
             ->get()
             ->unique('purchasable_id')
             ->sortBy('purchasable.product.title');
 
-        $applications = $request->user()
+        $applications = $user
             ->assignmentsWithoutRenewals()
-            ->with(['purchasable.product', 'purchasable.media', 'licenses.activations'])
+            ->with(['purchase.user', 'purchasable.product', 'purchasable.media', 'licenses.activations'])
             ->whereHas('licenses')
             ->get()
             ->sortBy('purchasable.product.title')
             ->groupBy('purchasable.product_id');
 
-        return view('front.profile.purchases', compact('courses', 'applications'));
+        $assignedAwayPurchases = $user
+            ->purchasesWithoutRenewals()
+            ->with([
+                'assignments' => fn ($query) => $query
+                    ->where('user_id', '!=', $user->id)
+                    ->with(['purchasable.product', 'user']),
+                'receipt',
+            ])
+            ->whereHas('assignments', fn (Builder $query) => $query->where('user_id', '!=', $user->id))
+            ->get()
+            ->sortByDesc(fn (Purchase $purchase) => $purchase->receipt->paid_at ?? $purchase->created_at);
+
+        return view('front.profile.purchases', compact('courses', 'applications', 'assignedAwayPurchases'));
     }
 }
